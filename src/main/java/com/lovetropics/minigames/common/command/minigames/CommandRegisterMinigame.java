b057f0d081f431a2fb06ffe9219695d67cdf0003
gegy1000 <gegy1000@gmail.com>
2020-09-28 20:51:25 +0200

    parse register role argument dynamically

--- src/main/java/com/lovetropics/minigames/common/command/minigames/CommandRegisterMinigame.java
+++ src/main/java/com/lovetropics/minigames/common/command/minigames/CommandRegisterMinigame.java
@@ -3,34 +3,53 @@ package com.lovetropics.minigames.common.command.minigames;
 import com.lovetropics.minigames.common.minigames.MinigameManager;
 import com.lovetropics.minigames.common.minigames.PlayerRole;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.ISuggestionProvider;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
 
 import javax.annotation.Nullable;
 
+import static net.minecraft.command.Commands.argument;
 import static net.minecraft.command.Commands.literal;
 
 public class CommandRegisterMinigame {
+	private static final DynamicCommandExceptionType ROLE_NOT_VALID = new DynamicCommandExceptionType(key -> {
+		return new StringTextComponent("'" + key + "' is not a valid role");
+	});
+
 	public static void register(final CommandDispatcher<CommandSource> dispatcher) {
 		// @formatter:off
 		dispatcher.register(
 			literal("minigame")
 				.then(literal("register")
 					.executes(ctx -> registerAsRole(ctx, null))
-					.then(literal("participant")
+					.then(argument("role", StringArgumentType.word())
 						.requires(source -> source.hasPermissionLevel(2))
-						.executes(ctx -> registerAsRole(ctx, PlayerRole.PARTICIPANT))
-					)
-					.then(literal("spectator")
-						.requires(source -> source.hasPermissionLevel(2))
-						.executes(ctx -> registerAsRole(ctx, PlayerRole.SPECTATOR))
+						.suggests((context, builder) -> ISuggestionProvider.suggest(
+							PlayerRole.stream().map(PlayerRole::getKey),
+							builder
+						))
+						.executes(CommandRegisterMinigame::registerAsRole)
 					)
 				)
 				.then(literal("spectate").executes(ctx -> registerAsRole(ctx, PlayerRole.SPECTATOR)))
 		);
 		// @formatter:on
+	}
+
+	private static int registerAsRole(CommandContext<CommandSource> ctx) throws CommandSyntaxException {
+		String roleKey = StringArgumentType.getString(ctx, "role");
+		PlayerRole role = PlayerRole.byKey(roleKey);
+		if (role == null) {
+			throw ROLE_NOT_VALID.create(roleKey);
+		}
+
+		return registerAsRole(ctx, role);
 	}
 
 	private static int registerAsRole(CommandContext<CommandSource> ctx, @Nullable PlayerRole requestedRole) throws CommandSyntaxException {
--- src/main/java/com/lovetropics/minigames/common/minigames/PlayerRole.java
+++ src/main/java/com/lovetropics/minigames/common/minigames/PlayerRole.java
@@ -1,8 +1,41 @@
 package com.lovetropics.minigames.common.minigames;
 
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public enum PlayerRole {
-	PARTICIPANT,
-	SPECTATOR;
+	PARTICIPANT("participant"),
+	SPECTATOR("spectator");
 
 	public static final PlayerRole[] ROLES = values();
+	private static final Map<String, PlayerRole> BY_KEY = new Object2ObjectOpenHashMap<>(ROLES.length);
+
+	private final String key;
+
+	PlayerRole(String key) {
+		this.key = key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public static Stream<PlayerRole> stream() {
+		return Arrays.stream(ROLES);
+	}
+
+	@Nullable
+	public static PlayerRole byKey(String key) {
+		return BY_KEY.get(key);
+	}
+
+	static {
+		for (PlayerRole role : ROLES) {
+			BY_KEY.put(role.key, role);
+		}
+	}
 }
