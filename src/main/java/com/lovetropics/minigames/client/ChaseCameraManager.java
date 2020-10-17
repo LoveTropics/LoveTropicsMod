fa76727c3ec4cbfbe036f0c9a51b51881f2de61d
gegy1000 <gegy1000@gmail.com>
2020-10-17 22:27:34 +0200

    support free camera in chase camera mode and allow for zooming into first-person

--- src/main/java/com/lovetropics/minigames/client/ChaseCameraManager.java
+++ src/main/java/com/lovetropics/minigames/client/ChaseCameraManager.java
@@ -10,11 +10,10 @@ import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.ActiveRenderInfo;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.client.resources.SkinManager;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -37,9 +36,14 @@ import java.util.UUID;
 
 @Mod.EventBusSubscriber(modid = Constants.MODID, value = Dist.CLIENT)
 public final class ChaseCameraManager {
-	private static final double CHASE_DISTANCE = 16.0;
+	private static final double MAX_CHASE_DISTANCE = 16.0;
+
+	private static final int ENTRY_PADDING = 2;
+	private static final int ENTRY_SIZE = 12 + ENTRY_PADDING;
 
 	private static final Minecraft CLIENT = Minecraft.getInstance();
+
+	private static final int ZOOM_KEY = InputMappings.getInputByName("key.keyboard.left.control").getKeyCode();
 
 	private static Session session;
 	private static double accumulatedScroll;
@@ -47,9 +51,6 @@ public final class ChaseCameraManager {
 	public static void start(List<UUID> players) {
 		session = new Session(players);
 		startSpectating(players.get(0));
-
-		CLIENT.gameSettings.thirdPersonView = 2;
-		CLIENT.gameSettings.smoothCamera = true;
 	}
 
 	public static void stop() {
@@ -87,48 +88,96 @@ public final class ChaseCameraManager {
 			return;
 		}
 
-		if (world.getGameTime() % 10 == 0) {
-			UUID spectatedId = session.players.get(session.selectedPlayerIndex);
-			PlayerEntity spectatedPlayer = world.getPlayerByUuid(spectatedId);
+		// reset to free camera if the player is spectating themselves
+		if (CLIENT.getRenderViewEntity() == CLIENT.player) {
+			session.selectedPlayerIndex = -1;
+		}
 
-			// we need to send position updates to the server or we won't properly track chunks
-			if (spectatedPlayer != null) {
-				ClientPlayerEntity player = CLIENT.player;
-				player.setPosition(spectatedPlayer.getPosX(), spectatedPlayer.getPosY(), spectatedPlayer.getPosZ());
+		session.prevZoom = session.zoom;
+
+		if (session.isSpectating()) {
+			if (world.getGameTime() % 10 == 0) {
+				// we need to send position updates to the server or we won't properly track chunks
+				UUID spectatedId = session.players.get(session.selectedPlayerIndex);
+				PlayerEntity spectatedPlayer = world.getPlayerByUuid(spectatedId);
+
+				if (spectatedPlayer != null) {
+					ClientPlayerEntity player = CLIENT.player;
+					player.setPosition(spectatedPlayer.getPosX(), spectatedPlayer.getPosY(), spectatedPlayer.getPosZ());
+				}
 			}
+		} else {
+			// force player to maximum flying speed since scrollwheel has been stolen
+			CLIENT.player.abilities.setFlySpeed(0.2F);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onRenderTick(TickEvent.RenderTickEvent event) {
+		Session session = ChaseCameraManager.session;
+		if (session == null || event.phase == TickEvent.Phase.END) {
+			return;
+		}
+
+		if (session.isSpectating()) {
+			ClientPlayerEntity player = CLIENT.player;
+			Entity focusEntity = CLIENT.getRenderViewEntity();
+			focusEntity = focusEntity != null ? focusEntity : player;
+
+			boolean firstPerson = session.zoom <= 1e-2;
+
+			if (!firstPerson) {
+				CLIENT.gameSettings.smoothCamera = true;
+				CLIENT.gameSettings.thirdPersonView = 2;
+			} else {
+				CLIENT.gameSettings.smoothCamera = false;
+				CLIENT.gameSettings.thirdPersonView = 0;
+
+				player.rotationYaw = focusEntity.rotationYaw;
+				player.rotationPitch = focusEntity.rotationPitch;
+			}
+		} else {
+			CLIENT.gameSettings.smoothCamera = false;
+			CLIENT.gameSettings.thirdPersonView = 0;
 		}
 	}
 
 	@SubscribeEvent
 	public static void onPositionCamera(EntityViewRenderEvent.CameraSetup event) {
+		Session session = ChaseCameraManager.session;
 		if (session == null) {
 			return;
 		}
 
-		CLIENT.gameSettings.thirdPersonView = 2;
+		if (session.isSpectating()) {
+			ClientPlayerEntity player = CLIENT.player;
+			Entity focusEntity = CLIENT.getRenderViewEntity();
+			focusEntity = focusEntity != null ? focusEntity : player;
 
-		float partialTicks = (float) event.getRenderPartialTicks();
+			float partialTicks = (float) event.getRenderPartialTicks();
+			double zoom = MathHelper.lerp(partialTicks, session.prevZoom, session.zoom);
+			boolean firstPerson = zoom <= 1e-2;
 
-		ClientPlayerEntity player = CLIENT.player;
-		Entity focusEntity = CLIENT.getRenderViewEntity();
-		focusEntity = focusEntity != null ? focusEntity : player;
+			if (!firstPerson) {
+				ActiveRenderInfo camera = event.getInfo();
 
-		ActiveRenderInfo camera = event.getInfo();
+				float yaw = player.getYaw(partialTicks);
+				float pitch = player.getPitch(partialTicks);
 
-		float yaw = player.getYaw(partialTicks);
-		float pitch = player.getPitch(partialTicks);
+				event.setYaw(yaw);
+				event.setPitch(pitch);
+				camera.setDirection(yaw, pitch);
 
-		event.setYaw(yaw);
-		event.setPitch(pitch);
-		camera.setDirection(yaw, pitch);
+				camera.setPosition(
+						MathHelper.lerp(partialTicks, focusEntity.prevPosX, focusEntity.getPosX()),
+						MathHelper.lerp(partialTicks, focusEntity.prevPosY, focusEntity.getPosY()) + focusEntity.getEyeHeight(),
+						MathHelper.lerp(partialTicks, focusEntity.prevPosZ, focusEntity.getPosZ())
+				);
 
-		camera.setPosition(
-				MathHelper.lerp(partialTicks, focusEntity.prevPosX, focusEntity.getPosX()),
-				MathHelper.lerp(partialTicks, focusEntity.prevPosY, focusEntity.getPosY()) + focusEntity.getEyeHeight(),
-				MathHelper.lerp(partialTicks, focusEntity.prevPosZ, focusEntity.getPosZ())
-		);
-
-		camera.movePosition(-camera.calcCameraDistance(CHASE_DISTANCE), 0.0, 0.0);
+				double distance = zoom * MAX_CHASE_DISTANCE;
+				camera.movePosition(-camera.calcCameraDistance(distance), 0.0, 0.0);
+			}
+		}
 	}
 
 	@SubscribeEvent
@@ -140,6 +189,19 @@ public final class ChaseCameraManager {
 
 		double delta = event.getScrollDelta();
 
+		boolean zoom = InputMappings.isKeyDown(CLIENT.getMainWindow().getHandle(), ZOOM_KEY);
+		if (zoom) {
+			onScrollZoom(session, delta);
+		} else {
+			onScrollSelection(session, delta);
+		}
+	}
+
+	private static void onScrollZoom(Session session, double delta) {
+		session.zoom = MathHelper.clamp(session.zoom - delta * 0.1, 0.0, 1.0);
+	}
+
+	private static void onScrollSelection(Session session, double delta) {
 		if (accumulatedScroll != 0.0 && Math.signum(delta) != Math.signum(accumulatedScroll)) {
 			accumulatedScroll = 0.0;
 		}
@@ -149,10 +211,15 @@ public final class ChaseCameraManager {
 		int scrollAmount = (int) accumulatedScroll;
 		if (scrollAmount != 0) {
 			int newIndex = session.selectedPlayerIndex - scrollAmount;
-			newIndex = MathHelper.clamp(newIndex, 0, session.players.size() - 1);
+			newIndex = MathHelper.clamp(newIndex, -1, session.players.size() - 1);
 
 			session.selectedPlayerIndex = newIndex;
-			startSpectating(session.players.get(newIndex));
+
+			if (newIndex != -1) {
+				startSpectating(session.players.get(newIndex));
+			} else {
+				stopSpectating();
+			}
 		}
 	}
 
@@ -175,31 +242,37 @@ public final class ChaseCameraManager {
 		RenderSystem.enableBlend();
 		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
 
-		int entryPadding = 2;
-		int entrySize = 12 + entryPadding;
-
-		int listHeight = entrySize * session.players.size();
-
-		FontRenderer fontRenderer = CLIENT.fontRenderer;
-		TextureManager textureManager = CLIENT.getTextureManager();
+		int listHeight = (session.players.size() + 1) * ENTRY_SIZE;
 
 		int minY = (window.getScaledHeight() - listHeight) / 2;
 
 		RenderSystem.disableTexture();
 
-		UUID selectedPlayer = session.players.get(session.selectedPlayerIndex);
-		GameProfile selectedProfile = session.getPlayerProfile(selectedPlayer);
-		if (selectedProfile != null) {
-			String name = selectedProfile.getName();
+		GameProfile localProfile = CLIENT.player.getGameProfile();
 
-			int width = entrySize + entryPadding + fontRenderer.getStringWidth(name);
-			int y = minY + session.selectedPlayerIndex * entrySize;
-			AbstractGui.fill(0, y - 1, width + 1, y + entrySize - 1, 0x80000000);
+		// TODO: could make this code good and have a list of generic entries that aren't specifically players..
+		//       but i don't feel like it. :)
+
+		// draw the selection box
+		String selectedName;
+		if (session.selectedPlayerIndex != -1) {
+			GameProfile profile = session.getPlayerProfile(session.players.get(session.selectedPlayerIndex));
+			selectedName = profile != null ? profile.getName() : "";
+		} else {
+			selectedName = "Free Camera";
 		}
 
+		int selectionWidth = ENTRY_SIZE + ENTRY_PADDING + CLIENT.fontRenderer.getStringWidth(selectedName);
+		int selectionY = minY + (session.selectedPlayerIndex + 1) * ENTRY_SIZE;
+		AbstractGui.fill(0, selectionY - 1, selectionWidth + 1, selectionY + ENTRY_SIZE - 1, 0x80000000);
+
+		// draw all the player entries
 		RenderSystem.enableTexture();
 
 		int y = minY;
+
+		renderPlayerEntry(session, "Free Camera", localProfile, y);
+		y += ENTRY_SIZE;
 
 		for (UUID player : session.players) {
 			GameProfile profile = session.getPlayerProfile(player);
@@ -208,18 +281,22 @@ public final class ChaseCameraManager {
 			}
 
 			String name = profile.getName();
-			ResourceLocation skin = session.getPlayerSkin(profile);
+			renderPlayerEntry(session, name, profile, y);
 
-			textureManager.bindTexture(skin);
-			AbstractGui.blit(entryPadding, y, 12, 12, 8.0F, 8.0F, 8, 8, 64, 64);
-			AbstractGui.blit(entryPadding, y, 12, 12, 40.0F, 8.0F, 8, 8, 64, 64);
-
-			fontRenderer.drawString(name, entrySize + entryPadding, y + entryPadding, 0xFFFFFFFF);
-
-			y += entrySize;
+			y += ENTRY_SIZE;
 		}
 
 		RenderSystem.popMatrix();
+	}
+
+	private static void renderPlayerEntry(Session session, String name, GameProfile profile, int y) {
+		ResourceLocation skin = session.getPlayerSkin(profile);
+
+		CLIENT.getTextureManager().bindTexture(skin);
+		AbstractGui.blit(ENTRY_PADDING, y, 12, 12, 8.0F, 8.0F, 8, 8, 64, 64);
+		AbstractGui.blit(ENTRY_PADDING, y, 12, 12, 40.0F, 8.0F, 8, 8, 64, 64);
+
+		CLIENT.fontRenderer.drawString(name, ENTRY_SIZE + ENTRY_PADDING, y + ENTRY_PADDING, 0xFFFFFFFF);
 	}
 
 	static class Session {
@@ -227,6 +304,9 @@ public final class ChaseCameraManager {
 		private final Map<UUID, GameProfile> playerProfiles = new HashMap<>();
 		private final Map<UUID, ResourceLocation> playerSkins = new HashMap<>();
 		int selectedPlayerIndex;
+
+		double zoom = 1.0;
+		double prevZoom = 1.0;
 
 		Session(List<UUID> players) {
 			this.players = players;
@@ -271,6 +351,10 @@ public final class ChaseCameraManager {
 			} else {
 				return DefaultPlayerSkin.getDefaultSkin(PlayerEntity.getUUID(profile));
 			}
+		}
+
+		boolean isSpectating() {
+			return selectedPlayerIndex != -1;
 		}
 	}
 }
