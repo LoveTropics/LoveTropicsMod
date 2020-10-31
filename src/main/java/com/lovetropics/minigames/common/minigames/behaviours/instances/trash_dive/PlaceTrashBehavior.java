724e73f8689a022fa3bece79ce39536a1d632276
tterrag <tterrag1098@gmail.com>
2020-10-31 17:21:21 -0400

    Update place trash behavior to use new position data files

--- src/main/java/com/lovetropics/minigames/common/minigames/behaviours/instances/trash_dive/PlaceTrashBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/minigames/behaviours/instances/trash_dive/PlaceTrashBehavior.java
@@ -1,99 +1,92 @@
 package com.lovetropics.minigames.common.minigames.behaviours.instances.trash_dive;
 
-import com.lovetropics.minigames.common.block.LoveTropicsBlocks;
-import com.lovetropics.minigames.common.block.TrashBlock;
-import com.lovetropics.minigames.common.block.TrashType;
-import com.lovetropics.minigames.common.map.MapRegion;
-import com.lovetropics.minigames.common.minigames.IMinigameInstance;
-import com.lovetropics.minigames.common.minigames.behaviours.IMinigameBehavior;
-import com.mojang.datafixers.Dynamic;
-import it.unimi.dsi.fastutil.longs.LongArrayList;
-import it.unimi.dsi.fastutil.longs.LongList;
-import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
-import it.unimi.dsi.fastutil.longs.LongSet;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.server.ServerWorld;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.Random;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import com.lovetropics.minigames.common.block.LoveTropicsBlocks;
+import com.lovetropics.minigames.common.block.TrashBlock;
+import com.lovetropics.minigames.common.block.TrashType;
+import com.lovetropics.minigames.common.minigames.IMinigameInstance;
+import com.lovetropics.minigames.common.minigames.behaviours.IMinigameBehavior;
+import com.mojang.datafixers.Dynamic;
+
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import net.minecraft.block.Blocks;
+import net.minecraft.resources.IResource;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
 
 public final class PlaceTrashBehavior implements IMinigameBehavior {
 	private static final Logger LOGGER = LogManager.getLogger(PlaceTrashBehavior.class);
 
 	private final TrashType[] trashTypes = TrashType.values();
 
-	private final int count;
-	private final String region;
+	private final ResourceLocation positionData;
+	private final int centerY;
+	private final int range;
+	private final int density;
 
 	private final LongSet trashBlocks = new LongOpenHashSet();
 
-	public PlaceTrashBehavior(int count, String region) {
-		this.count = count;
-		this.region = region;
+	public PlaceTrashBehavior(ResourceLocation positionData, int centerY, int range, int density) {
+		this.positionData = positionData;
+		this.centerY = centerY;
+		this.range = range;
+		this.density = density;
 	}
 
 	public static <T> PlaceTrashBehavior parse(Dynamic<T> root) {
-		int count = root.get("count").asInt(10);
-		String region = root.get("region").asString("");
-		return new PlaceTrashBehavior(count, region);
+		ResourceLocation positionData = new ResourceLocation(root.get("positionData").asString(""));
+		int centerY = root.get("centerY").asInt(75);
+		int range = root.get("range").asInt(50);
+		int density = root.get("density").asInt(4);
+		return new PlaceTrashBehavior(positionData, centerY, range, density);
 	}
 
 	@Override
 	public void onConstruct(IMinigameInstance minigame) {
-		List<MapRegion> regions = new ArrayList<>(minigame.getMapRegions().get(region));
-		if (regions.isEmpty()) {
-			LOGGER.warn("No matching regions for placing trash! Was given '{}'", region);
+		LongBuffer candidatePositions;
+		try (IResource res = minigame.getServer().getResourceManager().getResource(positionData)) {
+			InputStream in = res.getInputStream();
+			final byte[] data = new byte[8];
+			final ByteBuffer buf = ByteBuffer.allocate(in.available());
+			candidatePositions = buf.asLongBuffer();
+			while (in.read(data) > 0) {
+				buf.put(data);
+			}
+			candidatePositions.position(0);
+		} catch (Exception e) {
+			LOGGER.error("Unexpected error reading position data:", e);
 			return;
 		}
 
 		ServerWorld world = minigame.getWorld();
 		Random random = world.rand;
 
-		LongList volumes = new LongArrayList(regions.size());
-		for (MapRegion region : regions) {
-			volumes.add(getVolumeFor(world, region));
-		}
-
-		long totalVolume = 0;
-		for (MapRegion region : regions) {
-			totalVolume += region.getVolume();
-		}
-
-		// place trash weighted by the volumes of each region
-		int remaining = count;
-		for (int i = 0; i < regions.size(); i++) {
-			MapRegion region = regions.get(i);
-			long volume = volumes.getLong(i);
-			int amount = (int) (count * totalVolume / volume);
-
-			int j = 0;
-			while (j < amount) {
-				if (tryPlaceTrash(world, region)) {
-					j++;
-				}
-			}
-
-			remaining -= amount;
-		}
-
-		// we're doing integer division: place the remainder randomly
-		while (remaining > 0) {
-			MapRegion region = regions.get(random.nextInt(regions.size()));
-			if (tryPlaceTrash(world, region)) {
-				remaining--;
+		BlockPos.Mutable pos = new BlockPos.Mutable();
+		while (candidatePositions.hasRemaining()) {
+			pos.setPos(candidatePositions.get());
+			pos.setY(centerY);
+			int count = random.nextInt(density);
+			for (int i = 0; i < count; i++) {
+				pos.move(Direction.UP, (int) ((random.nextGaussian() / 3) * range));
+				tryPlaceTrash(world, pos);
+				pos.setY(centerY);
 			}
 		}
 	}
 
-	private boolean tryPlaceTrash(ServerWorld world, MapRegion region) {
+	private boolean tryPlaceTrash(ServerWorld world, BlockPos pos) {
 		Random random = world.rand;
-		BlockPos pos = region.sample(random);
-
 		if (world.getBlockState(pos).getBlock() == Blocks.WATER) {
 			TrashType trashType = trashTypes[random.nextInt(trashTypes.length)];
 			world.setBlockState(pos, LoveTropicsBlocks.TRASH.get(trashType).getDefaultState()
@@ -106,16 +99,6 @@ public final class PlaceTrashBehavior implements IMinigameBehavior {
 		}
 
 		return false;
-	}
-
-	private long getVolumeFor(ServerWorld world, MapRegion region) {
-		long volume = 0;
-		for (BlockPos pos : region) {
-			if (world.getBlockState(pos).getBlock() == Blocks.WATER) {
-				volume++;
-			}
-		}
-		return volume;
 	}
 
 	public LongSet getTrashBlocks() {
