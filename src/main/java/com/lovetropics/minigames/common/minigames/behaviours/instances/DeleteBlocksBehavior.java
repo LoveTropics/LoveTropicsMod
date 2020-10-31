c40e992cd7ec8bc02e05e076dc524441c106e030
gegy1000 <gegy1000@gmail.com>
2020-10-31 18:01:34 +0200

    delete blocks instead of activating lever and add behavior to add control for eliminating player

--- src/main/java/com/lovetropics/minigames/common/minigames/behaviours/instances/DeleteBlocksBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/minigames/behaviours/instances/DeleteBlocksBehavior.java
@@ -4,28 +4,27 @@ import com.lovetropics.minigames.common.map.MapRegion;
 import com.lovetropics.minigames.common.minigames.IMinigameInstance;
 import com.lovetropics.minigames.common.minigames.behaviours.IMinigameBehavior;
 import com.mojang.datafixers.Dynamic;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.LeverBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Collection;
 
-public final class ActivateLeversBehavior implements IMinigameBehavior {
+public final class DeleteBlocksBehavior implements IMinigameBehavior {
 	private final String regionKey;
 	private final long time;
 
 	private Collection<MapRegion> regions;
 
-	public ActivateLeversBehavior(String regionKey, long time) {
+	public DeleteBlocksBehavior(String regionKey, long time) {
 		this.regionKey = regionKey;
 		this.time = time;
 	}
 
-	public static <T> ActivateLeversBehavior parse(Dynamic<T> root) {
+	public static <T> DeleteBlocksBehavior parse(Dynamic<T> root) {
 		String regionKey = root.get("region").asString("");
 		long time = root.get("time").asLong(0);
-		return new ActivateLeversBehavior(regionKey, time);
+		return new DeleteBlocksBehavior(regionKey, time);
 	}
 
 	@Override
@@ -38,10 +37,7 @@ public final class ActivateLeversBehavior implements IMinigameBehavior {
 		if (time == minigame.ticks()) {
 			for (MapRegion region : regions) {
 				for (BlockPos pos : region) {
-					BlockState state = world.getBlockState(pos);
-					if (state.getBlock() instanceof LeverBlock) {
-						((LeverBlock) state.getBlock()).func_226939_d_(state, world, pos);
-					}
+					world.setBlockState(pos, Blocks.AIR.getDefaultState());
 				}
 			}
 		}
---
+++ src/main/java/com/lovetropics/minigames/common/minigames/behaviours/instances/EliminatePlayerControlBehavior.java
@@ -0,0 +1,24 @@
+package com.lovetropics.minigames.common.minigames.behaviours.instances;
+
+import com.lovetropics.minigames.common.minigames.IMinigameInstance;
+import com.lovetropics.minigames.common.minigames.PlayerRole;
+import com.lovetropics.minigames.common.minigames.behaviours.IMinigameBehavior;
+import com.mojang.datafixers.Dynamic;
+import net.minecraft.entity.player.ServerPlayerEntity;
+
+public final class EliminatePlayerControlBehavior implements IMinigameBehavior {
+	public static <T> EliminatePlayerControlBehavior parse(Dynamic<T> root) {
+		return new EliminatePlayerControlBehavior();
+	}
+
+	@Override
+	public void onConstruct(IMinigameInstance minigame) {
+		minigame.addControlCommand("eliminate", source -> {
+			ServerPlayerEntity player = source.asPlayer();
+			if (!minigame.getSpectators().contains(player.getUniqueID())) {
+				minigame.addPlayer(player, PlayerRole.SPECTATOR);
+				player.setHealth(20.0F);
+			}
+		});
+	}
+}
