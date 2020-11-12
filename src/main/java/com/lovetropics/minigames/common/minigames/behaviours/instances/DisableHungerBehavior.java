6a3d3f5b20b625851ad7b115d3c5549e92dd4417
gegy1000 <gegy1000@gmail.com>
2020-11-12 21:26:15 +0200

    fix crash from DisableHungerBehavior

--- src/main/java/com/lovetropics/minigames/common/minigames/behaviours/instances/DisableHungerBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/minigames/behaviours/instances/DisableHungerBehavior.java
@@ -4,8 +4,17 @@ import com.lovetropics.minigames.common.minigames.IMinigameInstance;
 import com.lovetropics.minigames.common.minigames.behaviours.IMinigameBehavior;
 import com.mojang.datafixers.Dynamic;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.FoodStats;
 
 public final class DisableHungerBehavior implements IMinigameBehavior {
+	private final CompoundNBT foodStats = new CompoundNBT();
+
+	public DisableHungerBehavior() {
+		FoodStats foodStats = new FoodStats();
+		foodStats.write(this.foodStats);
+	}
+
 	public static <T> DisableHungerBehavior parse(Dynamic<T> root) {
 		return new DisableHungerBehavior();
 	}
@@ -13,8 +22,7 @@ public final class DisableHungerBehavior implements IMinigameBehavior {
 	@Override
 	public void onParticipantUpdate(IMinigameInstance minigame, ServerPlayerEntity player) {
 		if (player.ticksExisted % 20 == 0) {
-			player.getFoodStats().setFoodLevel(20);
-			player.getFoodStats().setFoodSaturationLevel(5.0F);
+			player.getFoodStats().read(this.foodStats);
 		}
 	}
 }
