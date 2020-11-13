c213028d0541233512d2f1a4ace6adf2f2f95c27
gegy1000 <gegy1000@gmail.com>
2020-11-13 14:09:32 +0200

    never asked you to not compile did i

--- src/main/java/com/lovetropics/minigames/common/command/minigames/CommandMinigameDonate.java
+++ src/main/java/com/lovetropics/minigames/common/command/minigames/CommandMinigameDonate.java
@@ -1,6 +1,6 @@
 package com.lovetropics.minigames.common.command.minigames;
 
-import com.lovetropics.minigames.common.game_actions.DonationPackageGameAction;
+import com.lovetropics.minigames.common.game_actions.GamePackage;
 import com.lovetropics.minigames.common.minigames.IMinigameInstance;
 import com.lovetropics.minigames.common.minigames.MinigameManager;
 import com.lovetropics.minigames.common.minigames.behaviours.IMinigameBehavior;
@@ -9,8 +9,6 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandSource;
-
-import java.util.UUID;
 
 import static net.minecraft.command.Commands.argument;
 import static net.minecraft.command.Commands.literal;
@@ -32,9 +30,9 @@ public class CommandMinigameDonate {
 	private static int execute(CommandSource source, String packageType) throws CommandSyntaxException {
 		final IMinigameInstance instance = MinigameManager.getInstance().getActiveMinigame();
 		if (instance != null) {
-			DonationPackageGameAction action = new DonationPackageGameAction(UUID.randomUUID(), packageType, "0", source.getName(), source.asPlayer().getUniqueID());
+			GamePackage gamePackage = new GamePackage(packageType, source.getName(), source.asPlayer().getUniqueID());
 			for (IMinigameBehavior behavior : instance.getBehaviors()) {
-				behavior.onGamePackageReceived(instance, action);
+				behavior.onGamePackageReceived(instance, gamePackage);
 			}
 			return Command.SINGLE_SUCCESS;
 		}
