159984c5f3d23a8a5e23f09916707cb4e7784eeb
tterrag <tterrag1098@gmail.com>
2020-11-13 00:24:56 -0500

    Add more lenient displaytest

--- src/main/java/com/lovetropics/minigames/Constants.java
+++ src/main/java/com/lovetropics/minigames/Constants.java
@@ -3,4 +3,5 @@ package com.lovetropics.minigames;
 public class Constants {
 
     public static final String MODID = "ltminigames";
+    public static final String PROTOCOL_VERSION = "1";
 }
--- src/main/java/com/lovetropics/minigames/common/network/LTNetwork.java
+++ src/main/java/com/lovetropics/minigames/common/network/LTNetwork.java
@@ -1,24 +1,25 @@
 package com.lovetropics.minigames.common.network;
 
 import com.lovetropics.minigames.Constants;
+import com.lovetropics.minigames.LoveTropics;
+import com.lovetropics.minigames.client.minigame.ClientMinigameMessage;
 import com.lovetropics.minigames.client.minigame.ClientRoleMessage;
 import com.lovetropics.minigames.client.minigame.PlayerCountsMessage;
-import com.lovetropics.minigames.client.minigame.ClientMinigameMessage;
 import com.lovetropics.minigames.common.network.map.AddWorkspaceRegionMessage;
 import com.lovetropics.minigames.common.network.map.SetWorkspaceMessage;
 import com.lovetropics.minigames.common.network.map.UpdateWorkspaceRegionMessage;
+
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public final class LTNetwork {
-	private static final String PROTOCOL_VERSION = "1";
 
 	public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
 			new ResourceLocation(Constants.MODID, "main"),
-			() -> PROTOCOL_VERSION,
-			PROTOCOL_VERSION::equals,
-			PROTOCOL_VERSION::equals
+			() -> LoveTropics.getCompatVersion(),
+			LoveTropics::isCompatibleVersion,
+			LoveTropics::isCompatibleVersion
 	);
 
 	public static void register() {
