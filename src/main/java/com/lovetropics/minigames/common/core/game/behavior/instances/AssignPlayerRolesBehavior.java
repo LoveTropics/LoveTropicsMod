4e80c59775ad51f90a9aacd664ef95ce9a3d647a
Gegy <gegy1000@gmail.com>
2021-10-09 22:05:36 +0200

    delay role assigning to avoid inventory being cleared

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/AssignPlayerRolesBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/AssignPlayerRolesBehavior.java
@@ -8,20 +8,30 @@ import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvent
 import com.lovetropics.minigames.common.core.game.player.PlayerRole;
 import com.lovetropics.minigames.common.core.game.util.TeamAllocator;
 import com.mojang.serialization.Codec;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import net.minecraft.entity.player.ServerPlayerEntity;
+
+import java.util.Map;
 
 public final class AssignPlayerRolesBehavior implements IGameBehavior {
 	public static final Codec<AssignPlayerRolesBehavior> CODEC = Codec.unit(AssignPlayerRolesBehavior::new);
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
+		Map<ServerPlayerEntity, PlayerRole> roles = new Reference2ObjectOpenHashMap<>();
+
 		events.listen(GamePhaseEvents.CREATE, () -> {
 			TeamAllocator<PlayerRole, ServerPlayerEntity> allocator = game.getLobby().getPlayers().createRoleAllocator();
 			allocator.setSizeForTeam(PlayerRole.PARTICIPANT, game.getDefinition().getMaximumParticipantCount());
 
 			game.invoker(GamePlayerEvents.ALLOCATE_ROLES).onAllocateRoles(allocator);
 
-			allocator.allocate(game::setPlayerRole);
+			allocator.allocate(roles::put);
+		});
+
+		events.listen(GamePlayerEvents.ADD, player -> {
+			PlayerRole role = roles.remove(player);
+			game.setPlayerRole(player, role);
 		});
 	}
 }
