3d856bdd2ca058b5c4a3f52c997434fbdbc012a5
Gegy <gegy1000@gmail.com>
2021-10-10 11:08:47 +0200

    allow lobby to be closed via command

--- src/main/java/com/lovetropics/minigames/common/core/command/game/ManageGameLobbyCommand.java
+++ src/main/java/com/lovetropics/minigames/common/core/command/game/ManageGameLobbyCommand.java
@@ -6,6 +6,7 @@ import com.lovetropics.minigames.common.core.game.GameResult;
 import com.lovetropics.minigames.common.core.game.config.GameConfig;
 import com.lovetropics.minigames.common.core.game.impl.MultiGameManager;
 import com.lovetropics.minigames.common.core.game.lobby.IGameLobby;
+import com.lovetropics.minigames.common.core.game.lobby.ILobbyManagement;
 import com.lovetropics.minigames.common.core.game.util.GameTexts;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
@@ -19,6 +20,7 @@ import static net.minecraft.command.Commands.literal;
 
 public class ManageGameLobbyCommand {
 	private static final SimpleCommandExceptionType NO_MANAGE_PERMISSION = new SimpleCommandExceptionType(GameTexts.Commands.noManagePermission());
+	private static final SimpleCommandExceptionType NOT_IN_LOBBY = new SimpleCommandExceptionType(GameTexts.Commands.notInLobby());
 
 	public static void register(CommandDispatcher<CommandSource> dispatcher) {
 		// @formatter:off
@@ -33,11 +35,20 @@ public class ManageGameLobbyCommand {
 					.then(GameLobbyArgument.argument("lobby")
 					.executes(ManageGameLobbyCommand::manageLobby)
 				))
+				.then(literal("manage")
+					.requires(source -> source.hasPermissionLevel(2))
+					.executes(ManageGameLobbyCommand::manageCurrentLobby)
+				)
 				.then(literal("enqueue")
 					.requires(source -> source.hasPermissionLevel(2))
 					.then(GameLobbyArgument.argument("lobby")
 					.then(GameConfigArgument.argument("game")
 					.executes(ManageGameLobbyCommand::enqueueGame)
+				))
+				.then(literal("close")
+					.requires(source -> source.hasPermissionLevel(2))
+					.then(GameLobbyArgument.argument("lobby")
+					.executes(ManageGameLobbyCommand::closeLobby)
 				))
 			)
         );
@@ -61,6 +72,20 @@ public class ManageGameLobbyCommand {
 		return Command.SINGLE_SUCCESS;
 	}
 
+	private static int manageCurrentLobby(CommandContext<CommandSource> context) throws CommandSyntaxException {
+		ServerPlayerEntity player = context.getSource().asPlayer();
+		IGameLobby lobby = MultiGameManager.INSTANCE.getLobbyFor(player);
+		if (lobby == null) {
+			throw NOT_IN_LOBBY.create();
+		}
+
+		if (!lobby.getManagement().startManaging(player)) {
+			throw NO_MANAGE_PERMISSION.create();
+		}
+
+		return Command.SINGLE_SUCCESS;
+	}
+
 	private static int manageLobby(CommandContext<CommandSource> context) throws CommandSyntaxException {
 		ServerPlayerEntity player = context.getSource().asPlayer();
 		IGameLobby lobby = GameLobbyArgument.get(context, "lobby");
@@ -77,6 +102,19 @@ public class ManageGameLobbyCommand {
 		GameConfig game = GameConfigArgument.get(context, "game");
 
 		lobby.getGameQueue().enqueue(game);
+
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private static int closeLobby(CommandContext<CommandSource> context) throws CommandSyntaxException {
+		IGameLobby lobby = GameLobbyArgument.get(context, "lobby");
+
+		ILobbyManagement management = lobby.getManagement();
+		if (management.canManage(context.getSource())) {
+			management.close();
+		} else {
+			throw NO_MANAGE_PERMISSION.create();
+		}
 
 		return Command.SINGLE_SUCCESS;
 	}
