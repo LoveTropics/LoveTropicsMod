559df974c93fdd94a7b724894e495ef5d4487cf0
Gegy <gegy1000@gmail.com>
2021-10-19 19:44:52 +0200

    individual win trigger on role change

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/IndividualWinTrigger.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/IndividualWinTrigger.java
@@ -2,23 +2,27 @@ package com.lovetropics.minigames.common.core.game.behavior.instances;
 
 import com.lovetropics.minigames.common.core.game.GameException;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
-import com.lovetropics.minigames.common.core.game.player.PlayerSet;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameLogicEvents;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
+import com.lovetropics.minigames.common.core.game.player.PlayerRole;
+import com.lovetropics.minigames.common.core.game.player.PlayerSet;
 import com.lovetropics.minigames.common.core.game.state.statistics.PlayerKey;
 import com.lovetropics.minigames.common.core.game.state.statistics.StatisticKey;
 import com.mojang.serialization.Codec;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.ActionResultType;
 
 public class IndividualWinTrigger implements IGameBehavior {
 	public static final Codec<IndividualWinTrigger> CODEC = Codec.unit(IndividualWinTrigger::new);
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) throws GameException {
-		events.listen(GamePlayerEvents.DEATH, (player, damageSource) -> {
+		events.listen(GamePlayerEvents.SET_ROLE, (player, role, lastRole) -> {
+			if (lastRole != PlayerRole.PARTICIPANT) {
+				return;
+			}
+
 			PlayerSet participants = game.getParticipants();
 			if (participants.size() == 1) {
 				ServerPlayerEntity winningPlayer = participants.iterator().next();
@@ -28,8 +32,6 @@ public class IndividualWinTrigger implements IGameBehavior {
 
 				game.getStatistics().global().set(StatisticKey.WINNING_PLAYER, PlayerKey.from(winningPlayer));
 			}
-
-			return ActionResultType.PASS;
 		});
 	}
 }
