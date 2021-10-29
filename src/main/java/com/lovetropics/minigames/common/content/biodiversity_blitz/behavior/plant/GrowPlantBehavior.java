b831fbbabda9c4a26c1e42890973d6a5086f5c4d
Gegy <gegy1000@gmail.com>
2021-10-29 22:51:26 +0200

    biodiversity blitz: grow plants with delay from place time

--- src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/behavior/plant/GrowPlantBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/behavior/plant/GrowPlantBehavior.java
@@ -5,6 +5,7 @@ import com.lovetropics.minigames.common.content.biodiversity_blitz.behavior.even
 import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.Plot;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.plant.Plant;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.plant.PlantType;
+import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.plant.state.PlantState;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
@@ -14,6 +15,7 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.BlockPos;
@@ -32,43 +34,67 @@ public final class GrowPlantBehavior implements IGameBehavior {
 	private final int time;
 	private final PlantType growInto;
 
+	private final List<Plant> growPlants = new ArrayList<>();
+
 	public GrowPlantBehavior(int time, PlantType growInto) {
 		this.time = time;
 		this.growInto = growInto;
 	}
 
-	// TODO: rather store the planted time than going on an interval
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
+		events.listen(BbPlantEvents.ADD, (player, plot, plant) -> {
+			plant.state().put(GrowTime.KEY, new GrowTime(game.ticks() + this.time));
+		});
+
 		events.listen(BbPlantEvents.TICK, (player, plot, plants) -> {
 			long ticks = game.ticks();
-			if (ticks % this.time != 0) return;
+			if (ticks % 20 != 0) return;
 
-			ServerWorld world = game.getWorld();
-
-			List<PlantSnapshot> removedPlants = new ArrayList<>();
-			for (Plant plant : plants) {
-				removedPlants.add(this.removePlantBlocks(world, plant));
-			}
-
-			for (PlantSnapshot snapshot : removedPlants) {
-				Plant plant = snapshot.plant;
-				plot.plants.removePlant(plant);
-
-				BlockPos origin = plant.coverage().getOrigin();
-				ActionResult<Plant> result = game.invoker(BbEvents.PLACE_PLANT).placePlant(player, plot, origin, this.growInto);
-				if (result.getType() != ActionResultType.SUCCESS) {
-					this.restoreSnapshot(world, plot, snapshot);
-				}
+			List<Plant> growPlants = this.collectPlantsToGrow(plants, ticks);
+			for (Plant plant : growPlants) {
+				this.tryGrowPlant(game, player, plot, plant);
 			}
 		});
 	}
 
-	private PlantSnapshot removePlantBlocks(ServerWorld world, Plant plant) {
+	private void tryGrowPlant(IGamePhase game, ServerPlayerEntity player, Plot plot, Plant plant) {
+		ServerWorld world = game.getWorld();
+
+		PlantSnapshot snapshot = this.removeAndSnapshot(world, plot, plant);
+
+		BlockPos origin = plant.coverage().getOrigin();
+		ActionResult<Plant> result = game.invoker(BbEvents.PLACE_PLANT).placePlant(player, plot, origin, this.growInto);
+		if (result.getType() != ActionResultType.SUCCESS) {
+			this.restoreSnapshot(world, plot, snapshot);
+
+			GrowTime growTime = plant.state(GrowTime.KEY);
+			if (growTime != null) {
+				growTime.next = game.ticks() + this.time;
+			}
+		}
+	}
+
+	private List<Plant> collectPlantsToGrow(List<Plant> plants, long ticks) {
+		List<Plant> result = this.growPlants;
+		result.clear();
+
+		for (Plant plant : plants) {
+			GrowTime growTime = plant.state(GrowTime.KEY);
+			if (growTime != null && ticks >= growTime.next) {
+				result.add(plant);
+			}
+		}
+
+		return result;
+	}
+
+	private PlantSnapshot removeAndSnapshot(ServerWorld world, Plot plot, Plant plant) {
+		plot.plants.removePlant(plant);
+
 		Long2ObjectMap<BlockState> blocks = new Long2ObjectOpenHashMap<>();
 		for (BlockPos pos : plant.coverage()) {
 			blocks.put(pos.toLong(), world.getBlockState(pos));
-
 			world.setBlockState(pos, Blocks.AIR.getDefaultState(), Constants.BlockFlags.BLOCK_UPDATE | Constants.BlockFlags.UPDATE_NEIGHBORS);
 		}
 
@@ -83,6 +109,16 @@ public final class GrowPlantBehavior implements IGameBehavior {
 			if (block != null) {
 				world.setBlockState(pos, block, Constants.BlockFlags.BLOCK_UPDATE | Constants.BlockFlags.UPDATE_NEIGHBORS);
 			}
+		}
+	}
+
+	static final class GrowTime {
+		static final PlantState.Key<GrowTime> KEY = PlantState.Key.create();
+
+		long next;
+
+		GrowTime(long next) {
+			this.next = next;
 		}
 	}
 
