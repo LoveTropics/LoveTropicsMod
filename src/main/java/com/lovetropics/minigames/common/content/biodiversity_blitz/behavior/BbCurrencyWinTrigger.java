d43b27477c07f5e37421b06a4bd7a6f7ca763c6b
Gegy <gegy1000@gmail.com>
2021-10-30 13:33:11 +0200

    biodiversity blitz: tiebreaker in win condition

--- src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/behavior/BbCurrencyWinTrigger.java
+++ src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/behavior/BbCurrencyWinTrigger.java
@@ -2,16 +2,26 @@ package com.lovetropics.minigames.common.content.biodiversity_blitz.behavior;
 
 import com.lovetropics.minigames.common.content.biodiversity_blitz.behavior.event.BbEvents;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.client_state.CurrencyTargetState;
+import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.CurrencyManager;
+import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.Plot;
+import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.PlotsState;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameLogicEvents;
+import com.lovetropics.minigames.common.core.game.behavior.event.GamePhaseEvents;
 import com.lovetropics.minigames.common.core.game.client_state.GameClientState;
 import com.lovetropics.minigames.common.core.game.state.statistics.PlayerKey;
 import com.lovetropics.minigames.common.core.game.state.statistics.StatisticKey;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.entity.player.ServerPlayerEntity;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 
 public final class BbCurrencyWinTrigger implements IGameBehavior {
 	public static final Codec<BbCurrencyWinTrigger> CODEC = RecordCodecBuilder.create(instance -> {
@@ -22,20 +32,46 @@ public final class BbCurrencyWinTrigger implements IGameBehavior {
 
 	private final int thresholdCurrency;
 
+	private final List<ServerPlayerEntity> winnerCandidates = new ArrayList<>();
+
 	public BbCurrencyWinTrigger(int thresholdCurrency) {
 		this.thresholdCurrency = thresholdCurrency;
 	}
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
+		PlotsState plots = game.getState().getOrThrow(PlotsState.KEY);
+		CurrencyManager currency = game.getState().getOrThrow(CurrencyManager.KEY);
+
 		CurrencyTargetState target = new CurrencyTargetState(this.thresholdCurrency);
 		GameClientState.applyGlobally(target, events);
 
 		events.listen(BbEvents.CURRENCY_CHANGED, (player, value, lastValue) -> {
 			if (value >= this.thresholdCurrency) {
-				this.triggerWin(game, player);
+				this.winnerCandidates.add(player);
 			}
 		});
+
+		events.listen(GamePhaseEvents.TICK, () -> {
+			List<ServerPlayerEntity> players = this.winnerCandidates;
+			if (!players.isEmpty()) {
+				ServerPlayerEntity player = this.selectWinningPlayer(plots, currency, players);
+				if (player != null) {
+					this.triggerWin(game, player);
+				}
+			}
+		});
+	}
+
+	@Nullable
+	private ServerPlayerEntity selectWinningPlayer(PlotsState plots, CurrencyManager currency, Collection<ServerPlayerEntity> players) {
+		Comparator<ServerPlayerEntity> comparator = Comparator.comparingInt(currency::get)
+				.thenComparingInt(player -> {
+					Plot plot = plots.getPlotFor(player);
+					return plot != null ? plot.nextCurrencyIncrement : 0;
+				});
+
+		return players.stream().max(comparator).orElse(null);
 	}
 
 	private void triggerWin(IGamePhase game, ServerPlayerEntity player) {
