ac3c3c707dc0c9c9118d2c8c15b7195d32f4b909
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-10-30 13:10:28 -0400

    Make zombies not babies

--- src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/entity/BbHuskEntity.java
+++ src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/entity/BbHuskEntity.java
@@ -5,9 +5,7 @@ import com.lovetropics.minigames.common.content.biodiversity_blitz.entity.ai.BbM
 import com.lovetropics.minigames.common.content.biodiversity_blitz.entity.ai.DestroyCropGoal;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.entity.ai.MoveToPumpkinGoal;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.Plot;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.MoverType;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
@@ -17,10 +15,15 @@ import net.minecraft.entity.monster.HuskEntity;
 import net.minecraft.entity.monster.ZombifiedPiglinEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.pathfinding.PathNavigator;
 import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class BbHuskEntity extends HuskEntity implements BbMobEntity {
 	private final BbMobBrain mobBrain;
@@ -58,6 +61,18 @@ public class BbHuskEntity extends HuskEntity implements BbMobEntity {
 	@Override
 	protected Vector3d maybeBackOffFromEdge(Vector3d offset, MoverType mover) {
 		return mobBrain.getPlotWalls().collide(this.getBoundingBox(), offset);
+	}
+
+	@Nullable
+	@Override
+	public ILivingEntityData onInitialSpawn(IServerWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason, @Nullable ILivingEntityData spawnDataIn, @Nullable CompoundNBT dataTag) {
+		if (this.rand.nextFloat() < 0.05F) {
+			this.setLeftHanded(true);
+		} else {
+			this.setLeftHanded(false);
+		}
+
+		return spawnDataIn;
 	}
 
 	@Override
