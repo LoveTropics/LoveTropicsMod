1a69f3ca25fa04fbd483c7331b6fc10cced6f533
Gegy <gegy1000@gmail.com>
2021-11-04 02:09:20 +0200

    swap_players_package distance threshold to swap nearby

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/donation/SwapPlayersPackageBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/donation/SwapPlayersPackageBehavior.java
@@ -5,8 +5,8 @@ import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePackageEvents;
-import com.lovetropics.minigames.common.core.game.behavior.event.GamePhaseEvents;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.math.vector.Vector3d;
@@ -16,38 +16,71 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SwapPlayersPackageBehavior implements IGameBehavior {
-	public static final Codec<SwapPlayersPackageBehavior> CODEC = Codec.unit(SwapPlayersPackageBehavior::new);
+	public static final Codec<SwapPlayersPackageBehavior> CODEC = RecordCodecBuilder.create(instance -> {
+		return instance.group(
+				Codec.DOUBLE.optionalFieldOf("distance_threshold", Double.MAX_VALUE).forGetter(c -> c.distanceThreshold)
+		).apply(instance, SwapPlayersPackageBehavior::new);
+	});
 
-	private int swapCountdown;
+	private final double distanceThreshold;
+
+	public SwapPlayersPackageBehavior(double distanceThreshold) {
+		this.distanceThreshold = distanceThreshold;
+	}
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
 		events.listen(GamePackageEvents.APPLY_PACKAGE, (player, sendingPlayer) -> {
-			if (swapCountdown <= 0) {
-				swapCountdown = 20;
-				return true;
+			if (this.distanceThreshold == Double.MAX_VALUE) {
+				this.shufflePlayers(game);
 			} else {
-				return false;
+				this.swapNearbyPlayers(game);
 			}
+			return true;
 		});
-		events.listen(GamePhaseEvents.TICK, () -> tick(game));
 	}
 
-	private void tick(IGamePhase game) {
-		if (swapCountdown <= 0) return;
+	private void shufflePlayers(IGamePhase game) {
+		List<ServerPlayerEntity> players = Lists.newArrayList(game.getParticipants());
+		Collections.shuffle(players);
 
-		if (--swapCountdown <= 0) {
-			List<ServerPlayerEntity> players = Lists.newArrayList(game.getParticipants());
-			Collections.shuffle(players);
+		List<Vector3d> playerPositions = players.stream()
+				.map(Entity::getPositionVec)
+				.collect(Collectors.toList());
 
-			List<Vector3d> playerPositions = players.stream()
-					.map(Entity::getPositionVec)
-					.collect(Collectors.toList());
+		for (int i = 0; i < players.size(); i++) {
+			final ServerPlayerEntity player = players.get(i);
+			final Vector3d teleportTo = playerPositions.get((i + 1) % playerPositions.size());
+			player.setPositionAndUpdate(teleportTo.x, teleportTo.y, teleportTo.z);
+		}
+	}
 
-			for (int i = 0; i < players.size(); i++) {
-				final ServerPlayerEntity player = players.get(i);
-				final Vector3d teleportTo = playerPositions.get((i + 1) % playerPositions.size());
-				player.setPositionAndUpdate(teleportTo.x, teleportTo.y, teleportTo.z);
+	private void swapNearbyPlayers(IGamePhase game) {
+		List<ServerPlayerEntity> players = Lists.newArrayList(game.getParticipants());
+		Collections.shuffle(players);
+
+		List<Vector3d> playerPositions = players.stream()
+				.map(Entity::getPositionVec)
+				.collect(Collectors.toList());
+
+		double distanceThreshold2 = distanceThreshold * distanceThreshold;
+
+		for (ServerPlayerEntity player : players) {
+			Vector3d closestPos = null;
+			double closestDistance2 = Double.MAX_VALUE;
+
+			for (Vector3d otherPos : playerPositions) {
+				double distance2 = player.getPositionVec().squareDistanceTo(otherPos);
+				if (distance2 > 0.01 && distance2 < distanceThreshold2) {
+					if (distance2 < closestDistance2) {
+						closestPos = otherPos;
+						closestDistance2 = distance2;
+					}
+				}
+			}
+
+			if (closestPos != null) {
+				player.setPositionAndUpdate(closestPos.x, closestPos.y, closestPos.z);
 			}
 		}
 	}
