4a44db392b91d4fbc3d80a2095f2e8c7ca69b0f3
Gegy <gegy1000@gmail.com>
2021-11-05 09:46:56 +0200

    the data fixer is tired

---
+++ src/main/java/com/lovetropics/minigames/mixin/DataFixesManagerMixin.java
@@ -0,0 +1,19 @@
+package com.lovetropics.minigames.mixin;
+
+import net.minecraft.util.datafix.DataFixesManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.concurrent.Executor;
+
+@Mixin(DataFixesManager.class)
+public class DataFixesManagerMixin {
+	/**
+	 * Makes DataFixerUpper lazy. Shh, don't tell.
+	 */
+	@ModifyArg(method = "createFixer", at = @At(value = "INVOKE", target = "Lcom/mojang/datafixers/DataFixerBuilder;build(Ljava/util/concurrent/Executor;)Lcom/mojang/datafixers/DataFixer;"))
+	private static Executor replaceFixerOptimizationExecutor(Executor executor) {
+		return task -> {};
+	}
+}
