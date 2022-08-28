e1b1a4bad0d790da1baa171555f7f48f1748aba3
Gegy <gegy.dev@gmail.com>
2022-08-28 22:01:55 +0200

    'while_in_region' trigger instead of command-specific behavior

---
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/trigger/WhileInRegionTrigger.java
@@ -0,0 +1,51 @@
+package com.lovetropics.minigames.common.core.game.behavior.instances.trigger;
+
+import com.lovetropics.lib.BlockBox;
+import com.lovetropics.minigames.common.core.game.GameException;
+import com.lovetropics.minigames.common.core.game.IGamePhase;
+import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
+import com.lovetropics.minigames.common.core.game.behavior.action.GameActionContext;
+import com.lovetropics.minigames.common.core.game.behavior.action.GameActionList;
+import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
+import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Map;
+
+public record WhileInRegionTrigger(Map<String, GameActionList> regionActions, int interval) implements IGameBehavior {
+	public static final Codec<WhileInRegionTrigger> CODEC = RecordCodecBuilder.create(i -> i.group(
+			Codec.unboundedMap(Codec.STRING, GameActionList.CODEC).fieldOf("regions").forGetter(WhileInRegionTrigger::regionActions),
+			Codec.INT.optionalFieldOf("interval", 20).forGetter(WhileInRegionTrigger::interval)
+	).apply(i, WhileInRegionTrigger::new));
+
+	@Override
+	public void register(IGamePhase game, EventRegistrar events) throws GameException {
+		for (GameActionList actions : regionActions.values()) {
+			actions.register(game, events);
+		}
+
+		events.listen(GamePlayerEvents.TICK, player -> {
+			if (player.tickCount % interval != 0) {
+				return;
+			}
+
+			for (Map.Entry<String, GameActionList> entry : regionActions.entrySet()) {
+				if (isPlayerInRegion(game, player, entry.getKey())) {
+					GameActionList actions = entry.getValue();
+					actions.apply(GameActionContext.EMPTY, player);
+				}
+			}
+		});
+	}
+
+	private boolean isPlayerInRegion(IGamePhase game, ServerPlayer player, String key) {
+		for (BlockBox region : game.getMapRegions().get(key)) {
+			if (region.contains(player.getX(), player.getY(), player.getZ())) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
