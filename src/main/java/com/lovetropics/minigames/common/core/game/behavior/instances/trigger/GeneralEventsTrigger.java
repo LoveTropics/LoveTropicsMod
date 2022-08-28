6e9dc7038c3df7044ff5703d5f2211d1dcf700ee
Gegy <gegy.dev@gmail.com>
2022-08-28 22:09:00 +0200

    'events' trigger to apply actions instead of commands

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/trigger/GeneralEventsTrigger.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/trigger/GeneralEventsTrigger.java
@@ -1,26 +1,32 @@
-package com.lovetropics.minigames.common.core.game.behavior.instances.command;
+package com.lovetropics.minigames.common.core.game.behavior.instances.trigger;
 
+import com.lovetropics.minigames.common.core.game.GameException;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
+import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
+import com.lovetropics.minigames.common.core.game.behavior.action.GameActionContext;
+import com.lovetropics.minigames.common.core.game.behavior.action.GameActionList;
 import com.lovetropics.minigames.common.core.game.behavior.event.*;
 import com.lovetropics.minigames.common.core.game.player.PlayerRole;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 
 import javax.annotation.Nullable;
-import java.util.List;
 import java.util.Map;
 
-public final class CommandEventsBehavior extends CommandInvokeMapBehavior {
-	public static final Codec<CommandEventsBehavior> CODEC = COMMANDS_CODEC.xmap(CommandEventsBehavior::new, c -> c.commands);
+// TODO: split up into separate trigger types
+public record GeneralEventsTrigger(Map<String, GameActionList> eventActions) implements IGameBehavior {
+	public static final Codec<GeneralEventsTrigger> CODEC = RecordCodecBuilder.create(i -> i.group(
+			Codec.unboundedMap(Codec.STRING, GameActionList.CODEC).fieldOf("actions").forGetter(GeneralEventsTrigger::eventActions)
+	).apply(i, GeneralEventsTrigger::new));
 
-	public CommandEventsBehavior(Map<String, List<String>> commands) {
-		super(commands);
-	}
-
-	// TODO: this can handle passing additional data to datapacks in the future too
 	@Override
-	protected void registerEvents(IGamePhase game, EventRegistrar events) {
+	public void register(IGamePhase game, EventRegistrar events) throws GameException {
+		for (GameActionList actions : eventActions.values()) {
+			actions.register(game, events);
+		}
+
 		this.invoke("ready");
 
 		events.listen(GamePhaseEvents.START, () -> this.invoke("start"));
@@ -57,8 +63,6 @@ public final class CommandEventsBehavior extends CommandInvokeMapBehavior {
 			return InteractionResult.PASS;
 		});
 
-		events.listen(GameLivingEntityEvents.TICK, entity -> this.invoke("entity_update", entity));
-
 		events.listen(GameLogicEvents.GAME_OVER, () -> this.invoke("game_over"));
 		events.listen(GameLogicEvents.PHASE_CHANGE, (phase, lastPhase) -> {
 			this.invoke("phase_finish/" + lastPhase.key());
@@ -79,6 +83,20 @@ public final class CommandEventsBehavior extends CommandInvokeMapBehavior {
 			this.invoke("player_participate", player);
 		} else if (role == PlayerRole.SPECTATOR) {
 			this.invoke("player_spectate", player);
+		}
+	}
+
+	private void invoke(String event) {
+		GameActionList actions = eventActions.get(event);
+		if (actions != null) {
+			actions.apply(GameActionContext.EMPTY);
+		}
+	}
+
+	private void invoke(String event, ServerPlayer player) {
+		GameActionList actions = eventActions.get(event);
+		if (actions != null) {
+			actions.apply(GameActionContext.EMPTY, player);
 		}
 	}
 }
