4a855cbe6d9022dcb23b011b766b0db7e84d1098
Gegy <gegy.dev@gmail.com>
2022-08-28 22:02:27 +0200

    'bind_controls' binds to actions instead of command

---
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/trigger/BindControlsBehavior.java
@@ -0,0 +1,37 @@
+package com.lovetropics.minigames.common.core.game.behavior.instances.trigger;
+
+import com.lovetropics.minigames.common.core.game.GameException;
+import com.lovetropics.minigames.common.core.game.IGamePhase;
+import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
+import com.lovetropics.minigames.common.core.game.behavior.action.GameActionContext;
+import com.lovetropics.minigames.common.core.game.behavior.action.GameActionList;
+import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
+import com.lovetropics.minigames.common.core.game.state.control.ControlCommand;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+
+import java.util.Map;
+
+public record BindControlsBehavior(Map<ControlCommand.Scope, Map<String, GameActionList>> scopedActions) implements IGameBehavior {
+	public static final Codec<BindControlsBehavior> CODEC = RecordCodecBuilder.create(i -> i.group(
+			Codec.unboundedMap(ControlCommand.Scope.CODEC, Codec.unboundedMap(Codec.STRING, GameActionList.CODEC)).fieldOf("actions").forGetter(BindControlsBehavior::scopedActions)
+	).apply(i, BindControlsBehavior::new));
+
+	@Override
+	public void register(IGamePhase game, EventRegistrar events) throws GameException {
+		scopedActions.forEach((scope, scopedActions) -> scopedActions.forEach((control, actions) -> {
+			actions.register(game, events);
+
+			game.getControlCommands().add(control, new ControlCommand(scope, source -> {
+				Entity entity = source.getEntity();
+				if (entity instanceof ServerPlayer player) {
+					actions.apply(GameActionContext.EMPTY, player);
+				} else {
+					actions.apply(GameActionContext.EMPTY);
+				}
+			}));
+		}));
+	}
+}
