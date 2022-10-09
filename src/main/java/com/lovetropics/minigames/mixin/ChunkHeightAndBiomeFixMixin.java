efabfc5dcef472e2becf16078ee6ee0ac88c52b2
Gegy <gegy.dev@gmail.com>
2022-10-09 20:00:04 +0200

    Fix: resolve lighting crash due by committing datafixer crimes
    Chunks were not correctly upgraded with world height changes due to not being the overworld, so we'd get errors from data being shifted wrongly.

---
+++ src/main/java/com/lovetropics/minigames/mixin/ChunkHeightAndBiomeFixMixin.java
@@ -0,0 +1,21 @@
+package com.lovetropics.minigames.mixin;
+
+import com.lovetropics.minigames.Constants;
+import net.minecraft.util.datafix.fixes.ChunkHeightAndBiomeFix;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+// I am sorry for what I must do.
+// We import our maps with new overworld dimension type settings, but the datafixer does not update the heights because it is not actually the overworld.
+// Let's pretend to be the overworld.
+@Mixin(ChunkHeightAndBiomeFix.class)
+public class ChunkHeightAndBiomeFixMixin {
+    @Redirect(method = "lambda$makeRule$10", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z"))
+    private boolean shouldUpgradeHeight(String overworld, Object dimension) {
+        if (((String) dimension).startsWith(Constants.MODID + ":")) {
+            return true;
+        }
+        return overworld.equals(dimension);
+    }
+}
