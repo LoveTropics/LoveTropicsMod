1464948f36e42397774b9371924e6023e2c5dba4
Gegy <gegy.dev@gmail.com>
2022-10-10 08:43:48 +0200

    Fix datafix fix by committing more crimes

---
+++ src/main/java/com/lovetropics/minigames/mixin/ChunkStorageMixin.java
@@ -0,0 +1,23 @@
+package com.lovetropics.minigames.mixin;
+
+import com.lovetropics.minigames.Constants;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.chunk.storage.ChunkStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+// I am sorry for what I must do.
+// We import our maps with new overworld dimension type settings, but the datafixer does not update the heights because it is not actually the overworld.
+// Let's pretend to be the overworld. (Handled by ChunkHeightAndBiomeFix)
+@Mixin(ChunkStorage.class)
+public class ChunkStorageMixin {
+    @Redirect(method = "injectDatafixingContext", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;putString(Ljava/lang/String;Ljava/lang/String;)V"))
+    private static void injectDimensionContext(CompoundTag instance, String key, String value) {
+        if (value.startsWith(Constants.MODID + ":")) {
+            instance.putString(key, "minecraft:overworld");
+        } else {
+            instance.putString(key, value);
+        }
+    }
+}
