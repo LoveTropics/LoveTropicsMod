882ded0b2c437d66f566c79616c33f43cd9703de
Gegy <gegy.dev@gmail.com>
2022-10-23 16:43:27 +0200

    Allow phases to have player-based conditions to speed-up progression

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/PhaseControllerBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/PhaseControllerBehavior.java
@@ -11,8 +11,8 @@ import com.lovetropics.minigames.common.core.game.state.GamePhase;
 import com.lovetropics.minigames.common.core.game.state.GamePhaseState;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.Mth;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class PhaseControllerBehavior implements IGameBehavior {
@@ -22,6 +22,7 @@ public class PhaseControllerBehavior implements IGameBehavior {
 	).apply(i, PhaseControllerBehavior::new));
 
 	private GamePhaseState phaseState;
+	private int initialPlayerCount;
 
 	private final List<PhaseDefinition> phases;
 	private int phaseIndex;
@@ -61,12 +62,18 @@ public class PhaseControllerBehavior implements IGameBehavior {
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
+		initialPlayerCount = game.getParticipants().size();
+
 		events.listen(GamePhaseEvents.TICK, () -> {
 			if (hasFinishedPhases) {
 				return;
 			}
 			PhaseDefinition phase = phases.get(phaseIndex);
-			phaseProgress += 1.0 / phase.length();
+
+			boolean advancing = phase.advanceConditions().test(game, initialPlayerCount);
+			float speedFactor = advancing ? phase.advanceSpeedFactor() : 0.0f;
+			phaseProgress += (1.0 / phase.length()) * speedFactor;
+
 			if (phaseProgress > 1.0) {
 				if (!nextPhase(game)) {
 					hasFinishedPhases = true;
@@ -77,10 +84,30 @@ public class PhaseControllerBehavior implements IGameBehavior {
 		});
 	}
 
-	private record PhaseDefinition(GamePhase phase, int length) {
+	private record PhaseDefinition(GamePhase phase, int length, float advanceSpeedFactor, AdvanceConditions advanceConditions) {
 		public static final Codec<PhaseDefinition> CODEC = RecordCodecBuilder.create(i -> i.group(
 				GamePhase.CODEC.fieldOf("key").forGetter(PhaseDefinition::phase),
-				Codec.INT.fieldOf("length_in_ticks").forGetter(PhaseDefinition::length)
+				Codec.INT.fieldOf("length_in_ticks").forGetter(PhaseDefinition::length),
+				Codec.FLOAT.optionalFieldOf("advance_speed_factor", 4.0f).forGetter(PhaseDefinition::advanceSpeedFactor),
+				AdvanceConditions.CODEC.optionalFieldOf("advance_conditions", AdvanceConditions.DEFAULT).forGetter(PhaseDefinition::advanceConditions)
 		).apply(i, PhaseDefinition::new));
+	}
+
+	private record AdvanceConditions(
+			float belowPlayerPercentage,
+			int belowPlayerCount
+	) {
+		public static final Codec<AdvanceConditions> CODEC = RecordCodecBuilder.create(i -> i.group(
+				Codec.FLOAT.optionalFieldOf("below_player_percentage", -1.0f).forGetter(AdvanceConditions::belowPlayerPercentage),
+				Codec.INT.optionalFieldOf("below_player_count", -1).forGetter(AdvanceConditions::belowPlayerCount)
+		).apply(i, AdvanceConditions::new));
+
+		public static final AdvanceConditions DEFAULT = new AdvanceConditions(-1.0f, -1);
+
+		public boolean test(IGamePhase game, int initialPlayerCount) {
+			int playerCount = game.getParticipants().size();
+			float playerPercentage = (float) playerCount / initialPlayerCount;
+			return playerPercentage <= belowPlayerPercentage || playerCount <= belowPlayerCount;
+		}
 	}
 }
