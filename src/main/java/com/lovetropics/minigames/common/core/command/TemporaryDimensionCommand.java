1de64b21c43317cd32b0745b1c1021c330827782
Ashhhleyyy <ash@ashhhleyyy.dev>
2022-10-31 20:54:17 +0000

    feat: command to list and close temporary dimensions (fixes #140)

---
+++ src/main/java/com/lovetropics/minigames/common/core/command/TemporaryDimensionCommand.java
@@ -0,0 +1,80 @@
+package com.lovetropics.minigames.common.core.command;
+
+import com.lovetropics.minigames.common.core.dimension.RuntimeDimensionHandle;
+import com.lovetropics.minigames.common.core.dimension.RuntimeDimensions;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.arguments.DimensionArgument;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
+
+public class TemporaryDimensionCommand {
+    private static final DynamicCommandExceptionType NOT_TEMPORARY_DIMENSION = new DynamicCommandExceptionType(o ->
+        new TextComponent("Not a temporary dimension: '" + o + "'"));
+
+    private static final DynamicCommandExceptionType DIMENSION_HAS_PLAYERS = new DynamicCommandExceptionType(o ->
+            new TextComponent("'" + o + "' contains players, use '/temporary-dimension close " + o + " force' to close anyway."));
+
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        // @formatter:off
+        dispatcher.register(
+                literal("temporary-dimension")
+                        .requires(source -> source.hasPermission(2))
+                        .then(literal("list")
+                                .executes(TemporaryDimensionCommand::listTemporaryDimensions))
+                        .then(literal("close")
+                                .then(argument("dimension", DimensionArgument.dimension())
+                                        .executes(ctx -> closeDimension(ctx, false))
+                                        .then(literal("force")
+                                                .executes(ctx -> closeDimension(ctx, true)))))
+        );
+        // @formatter:on
+    }
+
+    private static int listTemporaryDimensions(CommandContext<CommandSourceStack> ctx) {
+        MinecraftServer server = ctx.getSource().getServer();
+        RuntimeDimensions runtimeDimensions = RuntimeDimensions.get(server);
+
+        if (runtimeDimensions.getTemporaryDimensions().isEmpty()) {
+            ctx.getSource().sendSuccess(new TextComponent("No temporary dimensions open!"), false);
+        }
+
+        for (var dimension : runtimeDimensions.getTemporaryDimensions()) {
+             ServerLevel world = server.getLevel(dimension);
+             if (world == null) continue;
+             ctx.getSource().sendSuccess(new TextComponent(dimension.location() + ": " + world.players().size() + " players"), false);
+        }
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int closeDimension(CommandContext<CommandSourceStack> ctx, boolean force) throws CommandSyntaxException {
+        MinecraftServer server = ctx.getSource().getServer();
+        RuntimeDimensions runtimeDimensions = RuntimeDimensions.get(server);
+
+        ServerLevel level = DimensionArgument.getDimension(ctx, "dimension");
+
+        if (!runtimeDimensions.isTemporaryDimension(level.dimension())) {
+            throw NOT_TEMPORARY_DIMENSION.create(level.dimension().location());
+        }
+
+        if (!force && !level.players().isEmpty()) {
+            throw DIMENSION_HAS_PLAYERS.create(level.dimension().location());
+        }
+
+        RuntimeDimensionHandle handle = runtimeDimensions.handleForTemporaryDimension(level.dimension());
+        handle.delete();
+
+        ctx.getSource().sendSuccess(new TextComponent("Closed '" + level.dimension().location() + "'"), false);
+
+        return Command.SINGLE_SUCCESS;
+    }
+}
