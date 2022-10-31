2212ecb70602ee310ee3a6372d6a24ba02ca87dc
Gegy <gegy.dev@gmail.com>
2022-10-31 22:34:18 +0100

    Add solid collisions for players disguised as falling blocks

---
+++ src/main/java/com/lovetropics/minigames/mixin/PlayerMixin.java
@@ -0,0 +1,25 @@
+package com.lovetropics.minigames.mixin;
+
+import com.lovetropics.minigames.common.core.diguise.DisguiseType;
+import com.lovetropics.minigames.common.core.diguise.PlayerDisguise;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Player.class)
+public abstract class PlayerMixin extends LivingEntity {
+	private PlayerMixin(EntityType<? extends LivingEntity> type, Level level) {
+		super(type, level);
+	}
+
+	@Override
+	public boolean canBeCollidedWith() {
+		DisguiseType disguise = PlayerDisguise.getDisguiseType((Player) (Object) this);
+		if (disguise != null && disguise.type == EntityType.FALLING_BLOCK) {
+			return true;
+		}
+		return super.canBeCollidedWith();
+	}
+}
