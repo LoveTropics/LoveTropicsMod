5a61359881b981bec96d2fe507c73bd66d3ccfad
Gegy <gegy.dev@gmail.com>
2022-11-01 22:05:01 +0100

    Allow chest placement to be limited per vertical chunk section

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/world/FillChestsByMarkerBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/world/FillChestsByMarkerBehavior.java
@@ -3,11 +3,14 @@ package com.lovetropics.minigames.common.core.game.behavior.instances.world;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.objects.ObjectList;
 import it.unimi.dsi.fastutil.objects.ObjectLists;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.SectionPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
@@ -21,6 +24,8 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
 public class FillChestsByMarkerBehavior extends ChunkGeneratingBehavior {
@@ -28,19 +33,22 @@ public class FillChestsByMarkerBehavior extends ChunkGeneratingBehavior {
 			ForgeRegistries.BLOCKS.getCodec().fieldOf("marker").forGetter(c -> c.marker),
 			SimpleWeightedRandomList.wrappedCodec(ResourceLocation.CODEC).fieldOf("loot_tables").forGetter(c -> c.lootTables),
 			Codec.FLOAT.optionalFieldOf("percentage", 1.0f).forGetter(c -> c.percentage),
-			Codec.INT.optionalFieldOf("max_per_chunk", Integer.MAX_VALUE).forGetter(c -> c.maxPerChunk)
+			Codec.INT.optionalFieldOf("max_per_chunk", Integer.MAX_VALUE).forGetter(c -> c.maxPerChunk),
+			Codec.INT.optionalFieldOf("max_per_section", Integer.MAX_VALUE).forGetter(c -> c.maxPerSection)
 	).apply(i, FillChestsByMarkerBehavior::new));
 
 	private final Block marker;
 	private final SimpleWeightedRandomList<ResourceLocation> lootTables;
 	private final float percentage;
 	private final int maxPerChunk;
+	private final int maxPerSection;
 
-	public FillChestsByMarkerBehavior(Block marker, SimpleWeightedRandomList<ResourceLocation> lootTables, float percentage, int maxPerChunk) {
+	public FillChestsByMarkerBehavior(Block marker, SimpleWeightedRandomList<ResourceLocation> lootTables, float percentage, int maxPerChunk, int maxPerSection) {
 		this.marker = marker;
 		this.lootTables = lootTables;
 		this.percentage = percentage;
 		this.maxPerChunk = maxPerChunk;
+		this.maxPerSection = maxPerSection;
 	}
 
 	@Override
@@ -58,20 +66,34 @@ public class FillChestsByMarkerBehavior extends ChunkGeneratingBehavior {
 			trimChests(world, chestPositions, index);
 		}
 
-		if (chestPositions.size() > maxPerChunk) {
-			trimChests(world, chestPositions, maxPerChunk);
-		}
+		trimChests(world, chestPositions, maxPerChunk);
 
+		for (ObjectList<BlockPos> sectionChests : partitionBySection(chestPositions)) {
+			trimChests(world, sectionChests, maxPerSection);
+
+			for (BlockPos pos : sectionChests) {
+				BlockPos belowPos = pos.below();
+				BlockState belowState = world.getBlockState(belowPos);
+				Direction facing = belowState.getValue(BlockStateProperties.HORIZONTAL_FACING);
+				lootTables.getRandomValue(random).ifPresent(lootTable -> {
+					setChest(world, belowPos, lootTable, facing);
+				});
+
+				world.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+			}
+		}
+	}
+
+	private static Collection<ObjectList<BlockPos>> partitionBySection(ObjectList<BlockPos> chestPositions) {
+		if (chestPositions.isEmpty()) {
+			return List.of();
+		}
+		Int2ObjectMap<ObjectList<BlockPos>> partitioned = new Int2ObjectArrayMap<>();
 		for (BlockPos pos : chestPositions) {
-			BlockPos belowPos = pos.below();
-			BlockState belowState = world.getBlockState(belowPos);
-			Direction facing = belowState.getValue(BlockStateProperties.HORIZONTAL_FACING);
-			lootTables.getRandomValue(random).ifPresent(lootTable -> {
-				setChest(world, belowPos, lootTable, facing);
-			});
-
-			world.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+			int sectionY = SectionPos.blockToSectionCoord(pos.getY());
+			partitioned.computeIfAbsent(sectionY, i -> new ObjectArrayList<>()).add(pos);
 		}
+		return partitioned.values();
 	}
 
 	private static void trimChests(ServerLevel level, ObjectList<BlockPos> positions, int count) {
