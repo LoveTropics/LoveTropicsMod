37de877eb4f63395516fb92c1592260391675331
Gegy <gegy.dev@gmail.com>
2022-11-02 12:47:06 +0100

    Add action_bar message type for show_title action

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/ShowTitleAction.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/ShowTitleAction.java
@@ -9,16 +9,18 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.SharedConstants;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetTitlesAnimationPacket;
 
 import java.util.Optional;
 
-public record ShowTitleAction(Optional<Component> title, Optional<Component> subtitle, int fadeIn, int stay, int fadeOut) implements IGameBehavior {
+public record ShowTitleAction(Optional<Component> title, Optional<Component> subtitle, Optional<Component> actionBar, int fadeIn, int stay, int fadeOut) implements IGameBehavior {
 	public static final Codec<ShowTitleAction> CODEC = RecordCodecBuilder.create(i -> i.group(
 			MoreCodecs.TEXT.optionalFieldOf("title").forGetter(ShowTitleAction::title),
 			MoreCodecs.TEXT.optionalFieldOf("subtitle").forGetter(ShowTitleAction::subtitle),
+			MoreCodecs.TEXT.optionalFieldOf("action_bar").forGetter(ShowTitleAction::actionBar),
 			Codec.INT.optionalFieldOf("fade_in", SharedConstants.TICKS_PER_SECOND / 4).forGetter(ShowTitleAction::fadeIn),
 			Codec.INT.optionalFieldOf("stay", SharedConstants.TICKS_PER_SECOND * 2).forGetter(ShowTitleAction::stay),
 			Codec.INT.optionalFieldOf("fade_out", SharedConstants.TICKS_PER_SECOND / 4).forGetter(ShowTitleAction::fadeOut)
@@ -30,6 +32,7 @@ public record ShowTitleAction(Optional<Component> title, Optional<Component> sub
 			target.connection.send(new ClientboundSetTitlesAnimationPacket(fadeIn, stay, fadeOut));
 			title.ifPresent(title -> target.connection.send(new ClientboundSetTitleTextPacket(title)));
 			subtitle.ifPresent(subtitle -> target.connection.send(new ClientboundSetSubtitleTextPacket(subtitle)));
+			actionBar.ifPresent(actionBar -> target.connection.send(new ClientboundSetActionBarTextPacket(actionBar)));
 			return true;
 		});
 	}
