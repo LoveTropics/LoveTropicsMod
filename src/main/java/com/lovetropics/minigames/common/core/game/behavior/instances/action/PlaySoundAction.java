1a8ae55e3388464255d448a5e5af4465abd0eede
Gegy <gegy.dev@gmail.com>
2022-11-02 14:30:23 +0100

    Add default sound to play_sound event to avoid errors if sound was not loaded

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/PlaySoundAction.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/PlaySoundAction.java
@@ -8,11 +8,12 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 
 public record PlaySoundAction(SoundEvent sound, float volume, float pitch) implements IGameBehavior {
 	public static final Codec<PlaySoundAction> CODEC = RecordCodecBuilder.create(i -> i.group(
-			Registry.SOUND_EVENT.byNameCodec().fieldOf("sound").forGetter(PlaySoundAction::sound),
+			Registry.SOUND_EVENT.byNameCodec().optionalFieldOf("sound", SoundEvents.ARROW_HIT_PLAYER).forGetter(PlaySoundAction::sound),
 			Codec.FLOAT.optionalFieldOf("volume", 1.0f).forGetter(PlaySoundAction::volume),
 			Codec.FLOAT.optionalFieldOf("pitch", 1.0f).forGetter(PlaySoundAction::pitch)
 	).apply(i, PlaySoundAction::new));
