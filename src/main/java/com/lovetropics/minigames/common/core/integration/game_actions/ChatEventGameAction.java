dafe5bc765eb03269d532847e35b0e0aaf8ece79
Gegy <gegy.dev@gmail.com>
2022-11-02 18:47:43 +0100

    Only include trigger name in chat event action from backend

--- src/main/java/com/lovetropics/minigames/common/core/integration/game_actions/ChatEventGameAction.java
+++ src/main/java/com/lovetropics/minigames/common/core/integration/game_actions/ChatEventGameAction.java
@@ -1,6 +1,5 @@
 package com.lovetropics.minigames.common.core.integration.game_actions;
 
-import com.lovetropics.lib.codec.MoreCodecs;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePackageEvents;
 import com.mojang.serialization.Codec;
@@ -8,64 +7,18 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.ChatFormatting;
 
-import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.List;
-import java.util.UUID;
-import java.util.function.Consumer;
-
-/**
- * Chat-caused event
- */
-public record ChatEventGameAction(String resultType, String title, List<PollEntry> entries) implements GameAction {
+public record ChatEventGameAction(String trigger) implements GameAction {
     public static final MapCodec<ChatEventGameAction> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
-            Codec.STRING.fieldOf("chat_event_type").forGetter(c -> c.resultType),
-            Codec.STRING.fieldOf("title").forGetter(c -> c.title),
-            MoreCodecs.sorted(PollEntry.CODEC.listOf(), Comparator.comparingInt(PollEntry::results).reversed()).fieldOf("options").forGetter(c -> c.entries)
+            Codec.STRING.fieldOf("trigger").forGetter(ChatEventGameAction::trigger)
     ).apply(i, ChatEventGameAction::new));
 
+    // TODO: Make GamePackage system less specific to packages
     @Override
     public boolean resolve(IGamePhase game, MinecraftServer server) {
-        if (entries.isEmpty()) {
-            return true;
-        }
+        GamePackage triggeredPackage = new GamePackage(trigger, null, null);
 
-        PollEntry winner = entries.get(0);
-        GamePackage winnerPackage = winner.asPackage();
-
-        int votes = 0;
-        for (PollEntry entry : entries) {
-            votes += entry.results;
-        }
-
-        final int totalVotes = votes;
-        Consumer<IGamePhase> preamble = g -> g.getAllPlayers().sendMessage(
-                new TextComponent(this.title).withStyle(ChatFormatting.BOLD, ChatFormatting.AQUA)
-                    .append(new TextComponent(
-                            " just completed! After " + totalVotes + " votes, chat decided on something to happen... Do you trust them to have been nice?"
-                    ).withStyle(ChatFormatting.GRAY))
-        );
-
-        InteractionResult result = game.invoker(GamePackageEvents.RECEIVE_PACKAGE).onReceivePackage(preamble, winnerPackage);
+        InteractionResult result = game.invoker(GamePackageEvents.RECEIVE_PACKAGE).onReceivePackage($ -> {}, triggeredPackage);
         return result == InteractionResult.SUCCESS;
-    }
-
-    public PollEntry winner() {
-        return entries.get(0);
-    }
-
-    public record PollEntry(String key, String packageType, int results) {
-        public static final Codec<PollEntry> CODEC = RecordCodecBuilder.create(i -> i.group(
-                Codec.STRING.fieldOf("key").forGetter(c -> c.key),
-                Codec.STRING.fieldOf("title").forGetter(c -> c.packageType),
-                Codec.INT.fieldOf("results").forGetter(c -> c.results)
-        ).apply(i, PollEntry::new));
-
-        public GamePackage asPackage() {
-            return new GamePackage(packageType, null, null);
-        }
     }
 }
