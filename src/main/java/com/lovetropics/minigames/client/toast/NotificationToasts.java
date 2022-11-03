425026f3dd29b3e9db123530d9487a2f4897a9e7
Gegy <gegy.dev@gmail.com>
2022-11-03 14:43:06 +0100

    Narrate notification toasts if enabled

--- src/main/java/com/lovetropics/minigames/client/toast/NotificationToasts.java
+++ src/main/java/com/lovetropics/minigames/client/toast/NotificationToasts.java
@@ -1,11 +1,13 @@
 package com.lovetropics.minigames.client.toast;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.chat.NarratorChatListener;
 import net.minecraft.network.chat.Component;
 
 final class NotificationToasts {
 	static void display(Component message, NotificationStyle style) {
 		Minecraft client = Minecraft.getInstance();
 		client.getToasts().addToast(new NotificationToast(message, style));
+		NarratorChatListener.INSTANCE.sayNow(message);
 	}
 }
