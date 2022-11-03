c4a2898f6470b90f1dd18858d4552c3fec41e7f1
Gegy <gegy.dev@gmail.com>
2022-11-03 18:27:07 +0100

    Fix: send close game event to tech stack when game_over is triggered

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/SetupTelemetryBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/SetupTelemetryBehavior.java
@@ -5,12 +5,15 @@ import com.lovetropics.minigames.common.core.game.GameStopReason;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
+import com.lovetropics.minigames.common.core.game.behavior.event.GameLogicEvents;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePhaseEvents;
 import com.lovetropics.minigames.common.core.game.state.GameStateMap;
 import com.lovetropics.minigames.common.core.game.util.GameTexts;
 import com.lovetropics.minigames.common.core.integration.GameInstanceTelemetry;
 import com.lovetropics.minigames.common.core.integration.Telemetry;
 import com.mojang.serialization.Codec;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public final class SetupTelemetryBehavior implements IGameBehavior {
 	public static final Codec<SetupTelemetryBehavior> CODEC = Codec.unit(SetupTelemetryBehavior::new);
@@ -37,7 +40,17 @@ public final class SetupTelemetryBehavior implements IGameBehavior {
 
 		events.listen(GamePhaseEvents.START, () -> telemetry.start(events));
 
+		AtomicBoolean finished = new AtomicBoolean();
+		events.listen(GameLogicEvents.GAME_OVER, () -> {
+			if (finished.compareAndSet(false, true)) {
+				telemetry.finish(game.getStatistics());
+			}
+		});
+
 		events.listen(GamePhaseEvents.STOP, reason -> {
+			if (!finished.compareAndSet(false, true)) {
+				return;
+			}
 			if (reason == GameStopReason.finished()) {
 				telemetry.finish(game.getStatistics());
 			} else {
