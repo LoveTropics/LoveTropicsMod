b4edc12b1964d6d0a05cbe67157218576046ba37
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2023-03-11 00:19:00 -0500

    Update BbTutorialHuskEntity.java

--- src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/entity/BbTutorialHuskEntity.java
+++ src/main/java/com/lovetropics/minigames/common/content/biodiversity_blitz/entity/BbTutorialHuskEntity.java
@@ -1,11 +1,9 @@
 package com.lovetropics.minigames.common.content.biodiversity_blitz.entity;
 
-import com.lovetropics.minigames.common.content.biodiversity_blitz.entity.ai.BbTargetPlayerGoal;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.entity.ai.DestroyCropGoal;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.entity.ai.MoveToPumpkinGoal;
 import com.lovetropics.minigames.common.content.biodiversity_blitz.plot.Plot;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.goal.ZombieAttackGoal;
 import net.minecraft.world.entity.monster.Husk;
 import net.minecraft.world.level.Level;
 
