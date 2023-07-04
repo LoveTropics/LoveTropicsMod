fb77cff8b1a75af81bd3cff2487bfd6107082359
Gegy <gegy.dev@gmail.com>
2023-07-04 22:51:48 +0200

    Use Vanilla's layouting system for SelectPlayerRoleScreen

--- src/main/java/com/lovetropics/minigames/client/lobby/select_role/SelectPlayerRoleScreen.java
+++ src/main/java/com/lovetropics/minigames/client/lobby/select_role/SelectPlayerRoleScreen.java
@@ -1,24 +1,25 @@
 package com.lovetropics.minigames.client.lobby.select_role;
 
-import com.lovetropics.minigames.client.screen.FlexUi;
-import com.lovetropics.minigames.client.screen.flex.Align;
-import com.lovetropics.minigames.client.screen.flex.Box;
-import com.lovetropics.minigames.client.screen.flex.Flex;
-import com.lovetropics.minigames.client.screen.flex.FlexSolver;
-import com.lovetropics.minigames.client.screen.flex.Layout;
 import com.lovetropics.minigames.common.core.game.util.GameTexts;
 import com.lovetropics.minigames.common.core.network.LoveTropicsNetwork;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.MultiLineTextWidget;
+import net.minecraft.client.gui.layouts.FrameLayout;
+import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.CommonColors;
 
 public final class SelectPlayerRoleScreen extends Screen {
 	private static final Component TITLE = GameTexts.Ui.selectPlayerRole()
 			.withStyle(ChatFormatting.BOLD, ChatFormatting.UNDERLINE);
 
 	// TODO: translate all the things
+	private static final Component PLAY_TEXT = Component.literal("Play");
+	private static final Component SPECTATE_TEXT = Component.literal("Spectate");
+
 	private static final Component[] TEXT = new Component[] {
 			Component.literal("Welcome to the game lobby!"),
 			Component.literal("Before the game, ")
@@ -32,11 +33,12 @@ public final class SelectPlayerRoleScreen extends Screen {
 			Component.literal("You will be prompted before each game in this lobby.").withStyle(ChatFormatting.GRAY)
 	};
 
-	private static final int PADDING = 4;
+	private static final int BUTTON_WIDTH = 100;
+	private static final int SPACING = 4;
 
 	private final int lobbyId;
 
-	private ScreenLayout layout;
+	private final GridLayout layout = new GridLayout().spacing(SPACING);
 	private boolean responded;
 
 	public SelectPlayerRoleScreen(int lobbyId) {
@@ -45,93 +47,61 @@ public final class SelectPlayerRoleScreen extends Screen {
 	}
 
 	@Override
-	protected void init() {
-		super.init();
-
-		layout = new ScreenLayout(this);
-
-		this.addRenderableWidget(FlexUi.createButton(layout.play, Component.literal("Play"), b -> {
-			this.acceptResponse(true);
-			this.onClose();
-		}));
-		this.addRenderableWidget(FlexUi.createButton(layout.spectate, Component.literal("Spectate"), b -> {
-			this.acceptResponse(false);
-			this.onClose();
-		}));
+	public Component getNarrationMessage() {
+		return CommonComponents.joinForNarration(title, CommonComponents.joinForNarration(TEXT));
 	}
 
-	private void acceptResponse(boolean play) {
-		if (!this.responded) {
-			LoveTropicsNetwork.CHANNEL.sendToServer(new SelectRoleMessage(lobbyId, play));
-			this.responded = true;
+	@Override
+	protected void init() {
+		GridLayout.RowHelper helper = layout.createRowHelper(1);
+		layout.defaultCellSetting().alignHorizontallyCenter();
+
+		for (Component line : TEXT) {
+			helper.addChild(new MultiLineTextWidget(line, font).setCentered(true));
 		}
+
+		GridLayout buttons = helper.addChild(new GridLayout().spacing(SPACING), helper.newCellSettings().paddingTop(SPACING));
+		GridLayout.RowHelper buttonsHelper = buttons.createRowHelper(2);
+		buttonsHelper.addChild(Button.builder(PLAY_TEXT, b -> {
+			sendResponse(true);
+			onClose();
+		}).width(BUTTON_WIDTH).build());
+		buttonsHelper.addChild(Button.builder(SPECTATE_TEXT, b -> {
+			sendResponse(false);
+			onClose();
+		}).width(BUTTON_WIDTH).build());
+
+		layout.visitWidgets(this::addRenderableWidget);
+		repositionElements();
+	}
+
+	@Override
+	protected void repositionElements() {
+		layout.arrangeElements();
+		FrameLayout.centerInRectangle(layout, 0, 0, width, height);
+	}
+
+	private void sendResponse(boolean play) {
+		if (!responded) {
+			LoveTropicsNetwork.CHANNEL.sendToServer(new SelectRoleMessage(lobbyId, play));
+			responded = true;
+		}
+	}
+
+	@Override
+	public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+		renderBackground(graphics);
+		super.render(graphics, mouseX, mouseY, partialTicks);
 	}
 
 	@Override
 	public void removed() {
 		super.removed();
-		this.acceptResponse(false);
-	}
-
-	@Override
-	public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-		this.renderBackground(graphics);
-
-		super.render(graphics, mouseX, mouseY, partialTicks);
-
-		this.renderText(graphics);
+		sendResponse(false);
 	}
 
 	@Override
 	public boolean shouldCloseOnEsc() {
 		return false;
-	}
-
-	private void renderText(GuiGraphics graphics) {
-		Box box = layout.text.content();
-
-		int lineHeight = font.lineHeight + PADDING;
-		int y = box.bottom() - lineHeight * TEXT.length;
-
-		for (Component line : TEXT) {
-			graphics.drawCenteredString(font, line, box.centerX(), y, CommonColors.WHITE);
-			y += lineHeight;
-		}
-	}
-
-	static final class ScreenLayout {
-		static final int BUTTON_WIDTH = 100;
-
-		final Layout text;
-		final Layout play;
-		final Layout spectate;
-
-		ScreenLayout(Screen screen) {
-			Flex root = new Flex().column();
-
-			Flex text = root.child().width(1.0F, Flex.Unit.PERCENT)
-					.grow(1.0F);
-
-			Flex buttons = root.child().row().width(1.0F, Flex.Unit.PERCENT).grow(1.0F);
-
-			Flex left = buttons.child().row().grow(1.0F).height(1.0F, Flex.Unit.PERCENT);
-			Flex right = buttons.child().row().grow(1.0F).height(1.0F, Flex.Unit.PERCENT);
-
-			Flex play = left.child()
-					.alignMain(Align.Main.END)
-					.margin(PADDING)
-					.size(BUTTON_WIDTH, 20);
-
-			Flex spectate = right.child()
-					.alignMain(Align.Main.START)
-					.margin(PADDING)
-					.size(BUTTON_WIDTH, 20);
-
-			FlexSolver.Results solve = new FlexSolver(new Box(screen)).apply(root);
-
-			this.text = solve.layout(text);
-			this.play = solve.layout(play);
-			this.spectate = solve.layout(spectate);
-		}
 	}
 }
