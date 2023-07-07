ce66591e078e290f35f524b19e4fa8ba126bc9ab
Gegy <gegy.dev@gmail.com>
2023-07-07 22:07:34 +0200

    Apply loaded game configs on main thread

--- src/main/java/com/lovetropics/minigames/common/core/game/config/GameConfigs.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/config/GameConfigs.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.lovetropics.lib.codec.CodecRegistry;
 import com.lovetropics.minigames.Constants;
-import com.lovetropics.minigames.mixin.TagManagerAccessor;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
@@ -13,9 +12,8 @@ import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.ReloadableServerResources;
-import net.minecraft.server.packs.resources.PreparableReloadListener;
 import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,8 +22,10 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 // TODO: Replace with a dynamic registry - currently blocked by not being able to /reload those
@@ -39,38 +39,41 @@ public final class GameConfigs {
 
 	@SubscribeEvent
 	public static void addReloadListener(AddReloadListenerEvent event) {
-		event.addListener((stage, resourceManager, preparationsProfiler, reloadProfiler, backgroundExecutor, gameExecutor) -> {
-			CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-				REGISTRY.clear();
-
-				DynamicOps<JsonElement> ops = RegistryOps.create(JsonOps.INSTANCE, event.getRegistryAccess());
-				BehaviorReferenceReader behaviorReader = new BehaviorReferenceReader(resourceManager);
-
-				FILE_TO_ID_CONVERTER.listMatchingResources(resourceManager).forEach((path, resource) -> {
-					try {
-						DataResult<GameConfig> result = loadConfig(ops, behaviorReader, path, resource);
-						result.result().ifPresent(config -> REGISTRY.register(config.id, config));
-
-						result.error().ifPresent(error -> {
-							LOGGER.error("Failed to load game config at {}: {}", path, error);
-						});
-					} catch (Exception e) {
-						LOGGER.error("Failed to load game config at {}", path, e);
-					}
-				});
-			}, backgroundExecutor);
-
-			return future.thenCompose(stage::wait);
-		});
+		RegistryAccess registryAccess = event.getRegistryAccess();
+		event.addListener((stage, resourceManager, preparationsProfiler, reloadProfiler, backgroundExecutor, gameExecutor) ->
+				CompletableFuture.supplyAsync(() -> listConfigs(registryAccess, resourceManager), backgroundExecutor)
+						.thenCompose(stage::wait)
+						.thenAcceptAsync(configs -> {
+							REGISTRY.clear();
+							for (GameConfig config : configs) {
+								REGISTRY.register(config.id, config);
+							}
+						}, gameExecutor));
 	}
 
-	private static DataResult<GameConfig> loadConfig(
-			DynamicOps<JsonElement> ops, BehaviorReferenceReader reader,
-			ResourceLocation path, Resource resource
-	) throws IOException {
-		try (InputStream input = resource.open()) {
-			JsonElement json = JsonParser.parseReader(new BufferedReader(new InputStreamReader(input)));
-			Codec<GameConfig> codec = GameConfig.codec(reader, FILE_TO_ID_CONVERTER.fileToId(path));
+	private static List<GameConfig> listConfigs(RegistryAccess registryAccess, ResourceManager resourceManager) {
+		List<GameConfig> configs = new ArrayList<>();
+
+		DynamicOps<JsonElement> ops = RegistryOps.create(JsonOps.INSTANCE, registryAccess);
+		BehaviorReferenceReader behaviorReader = new BehaviorReferenceReader(resourceManager);
+
+		FILE_TO_ID_CONVERTER.listMatchingResources(resourceManager).forEach((path, resource) -> {
+			try {
+				loadConfig(ops, behaviorReader, path, resource)
+						.resultOrPartial(error -> LOGGER.error("Failed to load game config at {}: {}", path, error))
+						.ifPresent(configs::add);
+			} catch (Exception e) {
+				LOGGER.error("Failed to load game config at {}", path, e);
+			}
+		});
+
+		return configs;
+	}
+
+	private static DataResult<GameConfig> loadConfig(DynamicOps<JsonElement> ops, BehaviorReferenceReader behaviorReader, ResourceLocation path, Resource resource) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.open(), StandardCharsets.UTF_8))) {
+			JsonElement json = JsonParser.parseReader(reader);
+			Codec<GameConfig> codec = GameConfig.codec(behaviorReader, FILE_TO_ID_CONVERTER.fileToId(path));
 			return codec.parse(ops, json);
 		}
 	}
