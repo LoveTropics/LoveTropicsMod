634d3b3b86a437c0ffa432ab60109483b20cc946
Gegy <gegy.dev@gmail.com>
2023-10-24 18:37:56 +0200

    Add sounds for STT4

--- src/main/java/com/lovetropics/minigames/SoundRegistry.java
+++ src/main/java/com/lovetropics/minigames/SoundRegistry.java
@@ -12,6 +12,9 @@ public class SoundRegistry {
 	public static final RegistryObject<SoundEvent> SWAP_PLAYERS = register("swap_players");
 	public static final RegistryObject<SoundEvent> PACKAGE_RECEIVE = register("package_receive");
 	public static final RegistryObject<SoundEvent> SABOTAGE_RECEIVE = register("sabotage_receive");
+	public static final RegistryObject<SoundEvent> ACID_FLASH_FLOODING_IMMINENT = register("stt4.acid_flash_flooding_imminent");
+	public static final RegistryObject<SoundEvent> FLASH_FLOODING_IMMINENT = register("stt4.flash_flooding_imminent");
+	public static final RegistryObject<SoundEvent> LAST_SHUTTLE_DEPARTING = register("stt4.last_shuttle_departing");
 
 	private static RegistryObject<SoundEvent> register(String name) {
 		return REGISTER.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Constants.MODID, name)));
