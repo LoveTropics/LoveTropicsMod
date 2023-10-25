47711ed193de11538e0306c882461a5799ee6d1a
Gegy <gegy.dev@gmail.com>
2023-10-25 13:57:44 +0200

    Support giving rewards from win trigger behaviors

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/team/TeamWinTrigger.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/team/TeamWinTrigger.java
@@ -1,5 +1,6 @@
 package com.lovetropics.minigames.common.core.game.behavior.instances.team;
 
+import com.lovetropics.lib.codec.MoreCodecs;
 import com.lovetropics.minigames.common.core.game.GameException;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
@@ -7,27 +8,36 @@ import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameLogicEvents;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
 import com.lovetropics.minigames.common.core.game.player.PlayerRole;
+import com.lovetropics.minigames.common.core.game.rewards.GameRewardsMap;
 import com.lovetropics.minigames.common.core.game.state.statistics.StatisticKey;
 import com.lovetropics.minigames.common.core.game.state.team.GameTeam;
 import com.lovetropics.minigames.common.core.game.state.team.GameTeamKey;
 import com.lovetropics.minigames.common.core.game.state.team.TeamState;
 import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Objects;
 
-public class TeamWinTrigger implements IGameBehavior {
-	public static final MapCodec<TeamWinTrigger> CODEC = MapCodec.unit(TeamWinTrigger::new);
-
-	private boolean winTriggered;
+public record TeamWinTrigger(List<ItemStack> rewards) implements IGameBehavior {
+	public static final MapCodec<TeamWinTrigger> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
+			MoreCodecs.ITEM_STACK.listOf().optionalFieldOf("rewards", List.of()).forGetter(TeamWinTrigger::rewards)
+	).apply(i, TeamWinTrigger::new));
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) throws GameException {
 		TeamState teamState = game.getInstanceState().getOrThrow(TeamState.KEY);
+		GameRewardsMap rewardsMap = game.getState().getOrThrow(GameRewardsMap.STATE);
+
+		MutableBoolean winTriggered = new MutableBoolean();
 
 		events.listen(GamePlayerEvents.SET_ROLE, (player, role, lastRole) -> {
-			if (lastRole != PlayerRole.PARTICIPANT || winTriggered) {
+			if (lastRole != PlayerRole.PARTICIPANT || winTriggered.isTrue()) {
 				return;
 			}
 
@@ -43,7 +53,7 @@ public class TeamWinTrigger implements IGameBehavior {
 					finalTeam = Objects.requireNonNull(teamState.getTeamByKey(playerTeam));
 				}
 
-				winTriggered = true;
+				winTriggered.setTrue();
 
 				Component winnerName = finalTeam.config().name().copy()
 						.withStyle(finalTeam.config().formatting());
@@ -51,6 +61,12 @@ public class TeamWinTrigger implements IGameBehavior {
 				game.invoker(GameLogicEvents.GAME_OVER).onGameOver();
 
 				game.getStatistics().global().set(StatisticKey.WINNING_TEAM, finalTeam.key());
+
+				for (ServerPlayer teamPlayer : teamState.getPlayersForTeam(finalTeam.key())) {
+					for (ItemStack reward : rewards) {
+						rewardsMap.give(teamPlayer, reward);
+					}
+				}
 			}
 		});
 	}
