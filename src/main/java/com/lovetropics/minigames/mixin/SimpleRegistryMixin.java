825fea136d86d911f82c96a87c7611c6b603dd80
Gegy <gegy.dev@gmail.com>
2023-10-25 21:40:37 +0200

    Fix: make mutable copy before modifying list

--- src/main/java/com/lovetropics/minigames/mixin/SimpleRegistryMixin.java
+++ src/main/java/com/lovetropics/minigames/mixin/SimpleRegistryMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -44,6 +45,7 @@ public class SimpleRegistryMixin<T> implements RegistryEntryRemover<T> {
             this.unregisteredIntrusiveHolders.remove(entry);
         }
         if (this.holdersInOrder != null) {
+            this.holdersInOrder = new ArrayList<>(this.holdersInOrder);
             this.holdersInOrder.set(rawId, null);
         }
 
