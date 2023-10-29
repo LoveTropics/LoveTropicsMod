5d31fd68ad93bf1a0626ba652065ea011a739f2f
Gegy <gegy.dev@gmail.com>
2023-10-29 16:21:29 +0100

    Add some Qottott texts

--- src/main/java/com/lovetropics/minigames/common/content/qottott/QottottTexts.java
+++ src/main/java/com/lovetropics/minigames/common/content/qottott/QottottTexts.java
@@ -11,5 +11,16 @@ public class QottottTexts {
 		KEYS.add("sidebar.instruction", "Pick up points!");
 		KEYS.add("sidebar.top_players", "Top Players:");
 		KEYS.add("time_remaining", "Time Remaining: %time%...");
+		KEYS.add("kit.picked", "You have selected: %s");
+		KEYS.add("kit.acrobat", "Acrobat");
+		KEYS.add("kit.assassin", "Assassin");
+		KEYS.add("kit.glass_cannon", "Glass Cannon");
+		KEYS.add("power_up.generic", "2x points for %seconds% seconds");
+		KEYS.add("power_up.speed", "2x points & increased Speed for %seconds% seconds");
+		KEYS.add("power_up.knockback", "2x points & increased Knockback for %seconds% seconds");
+		KEYS.add("power_up.knockback_resistance", "2x points & Knockback Resistance for %seconds% seconds");
+		KEYS.add("spawn.power_up", "%item% has spawned!");
+		KEYS.add("kill_bonus.tag", "KILL BONUS: %s");
+		KEYS.add("kill_bonus.announce", "%target% has a %count% point kill bonus!");
 	}
 }
