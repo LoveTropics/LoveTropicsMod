5aac4aa0a6c4a7c9590e4422c917c70936e0a267
Gegy <gegy.dev@gmail.com>
2023-11-01 01:06:59 +0100

    Allow player damage to be cancelled during a specific period

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/tweak/CancelPlayerDamageBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/tweak/CancelPlayerDamageBehavior.java
@@ -6,26 +6,31 @@ import com.lovetropics.minigames.common.core.game.behavior.GameBehaviorTypes;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
+import com.lovetropics.minigames.common.core.game.state.ProgressionPeriod;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.Optional;
+import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
-public record CancelPlayerDamageBehavior(boolean knockback) implements IGameBehavior {
+public record CancelPlayerDamageBehavior(boolean knockback, Optional<ProgressionPeriod> period) implements IGameBehavior {
 	public static final MapCodec<CancelPlayerDamageBehavior> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
-			Codec.BOOL.optionalFieldOf("knockback", false).forGetter(c -> c.knockback)
+			Codec.BOOL.optionalFieldOf("knockback", false).forGetter(CancelPlayerDamageBehavior::knockback),
+			ProgressionPeriod.CODEC.optionalFieldOf("period").forGetter(CancelPlayerDamageBehavior::period)
 	).apply(i, CancelPlayerDamageBehavior::new));
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
+		BooleanSupplier predicate = period.map(p -> p.createPredicate(game)).orElse(() -> true);
 		if (this.knockback) {
-			events.listen(GamePlayerEvents.DAMAGE_AMOUNT, (player, damageSource, amount, originalAmount) -> 0.0F);
+			events.listen(GamePlayerEvents.DAMAGE_AMOUNT, (player, damageSource, amount, originalAmount) -> predicate.getAsBoolean() ? 0.0F : amount);
 		} else {
-			events.listen(GamePlayerEvents.ATTACK, (player, target) -> target instanceof Player ? InteractionResult.FAIL : InteractionResult.PASS);
-			events.listen(GamePlayerEvents.DAMAGE, (player, damageSource, amount) -> InteractionResult.FAIL);
+			events.listen(GamePlayerEvents.ATTACK, (player, target) -> target instanceof Player && predicate.getAsBoolean() ? InteractionResult.FAIL : InteractionResult.PASS);
+			events.listen(GamePlayerEvents.DAMAGE, (player, damageSource, amount) -> predicate.getAsBoolean() ? InteractionResult.FAIL : InteractionResult.PASS);
 		}
 	}
 
