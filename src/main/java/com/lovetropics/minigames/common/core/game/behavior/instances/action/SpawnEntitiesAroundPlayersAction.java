be8e70aa29844ca4066fae98d66843bdcbf05150
Gegy <gegy.dev@gmail.com>
2023-11-01 17:00:30 +0100

    Add global maximum entity count option to spawn_entities_around_players action

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/SpawnEntitiesAroundPlayersAction.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/SpawnEntitiesAroundPlayersAction.java
@@ -15,6 +15,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 
 public class SpawnEntitiesAroundPlayersAction implements IGameBehavior {
@@ -24,7 +25,8 @@ public class SpawnEntitiesAroundPlayersAction implements IGameBehavior {
 			Codec.INT.optionalFieldOf("spawn_distance_min", 10).forGetter(c -> c.spawnDistanceMin),
 			Codec.INT.optionalFieldOf("spawn_distance_max", 20).forGetter(c -> c.spawnDistanceMax),
 			Codec.INT.optionalFieldOf("spawn_range_y", 10).forGetter(c -> c.spawnRangeY),
-			Codec.INT.optionalFieldOf("spawn_try_rate", 10).forGetter(c -> c.spawnsPerTick)
+			Codec.INT.optionalFieldOf("spawn_try_rate", 10).forGetter(c -> c.spawnsPerTick),
+			Codec.INT.optionalFieldOf("max_entity_count", Integer.MAX_VALUE).forGetter(c -> c.maxEntityCount)
 	).apply(i, SpawnEntitiesAroundPlayersAction::new));
 
 	private final EntityTemplate entity;
@@ -33,19 +35,26 @@ public class SpawnEntitiesAroundPlayersAction implements IGameBehavior {
 	private final int spawnDistanceMax;
 	private final int spawnRangeY;
 	private final int spawnsPerTick;
+	private final int maxEntityCount;
+	private int remainingEntityCount;
 	private final Object2IntMap<ServerPlayer> playerToAmountToSpawn = new Object2IntOpenHashMap<>();
 
-	public SpawnEntitiesAroundPlayersAction(final EntityTemplate entity, final int entityCount, final int spawnDistanceMin, final int spawnDistanceMax, final int spawnRangeY, final int spawnsPerTick) {
+	public SpawnEntitiesAroundPlayersAction(final EntityTemplate entity, final int entityCount, final int spawnDistanceMin, final int spawnDistanceMax, final int spawnRangeY, final int spawnsPerTick, final int maxEntityCount) {
 		this.entity = entity;
 		this.entityCountPerPlayer = entityCount;
 		this.spawnDistanceMin = spawnDistanceMin;
 		this.spawnDistanceMax = spawnDistanceMax;
 		this.spawnRangeY = spawnRangeY;
 		this.spawnsPerTick = spawnsPerTick;
+		this.maxEntityCount = maxEntityCount;
 	}
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
+		events.listen(GameActionEvents.APPLY, context -> {
+			remainingEntityCount = maxEntityCount;
+			return true;
+		});
 		events.listen(GameActionEvents.APPLY_TO_PLAYER, (context, player) -> {
 			playerToAmountToSpawn.put(player, entityCountPerPlayer);
 			return true;
@@ -64,13 +73,18 @@ public class SpawnEntitiesAroundPlayersAction implements IGameBehavior {
 
 				BlockPos pos = getSpawnableRandomPositionNear(game, entry.getKey().blockPosition(), spawnDistanceMin, spawnDistanceMax, spawnsPerTick, spawnRangeY);
 
-				if (pos != BlockPos.ZERO) {
+				if (pos != null) {
 					entry.setValue(entry.getIntValue() - 1);
 					if (entry.getIntValue() <= 0) {
 						it.remove();
 					}
 
 					entity.spawn(game.getWorld(), pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, 0.0f, 0.0f);
+
+					if (--remainingEntityCount == 0) {
+						playerToAmountToSpawn.clear();
+						return;
+					}
 				}
 
 			}
@@ -80,8 +94,9 @@ public class SpawnEntitiesAroundPlayersAction implements IGameBehavior {
 	/**
 	 * Tries to return a random spawnable position within the set distances up to a certain amount of attempts
 	 *
-	 * @return BlockPos.ZERO if it fails, otherwise a real position
+	 * @return null if it fails, otherwise a real position
 	 */
+	@Nullable
 	public BlockPos getSpawnableRandomPositionNear(final IGamePhase game, BlockPos pos, int minDist, int maxDist, int loopAttempts, int yRange) {
 		for (int i = 0; i < loopAttempts; i++) {
 			BlockPos posTry = pos.offset(game.getWorld().getRandom().nextInt(maxDist * 2) - maxDist,
@@ -92,7 +107,7 @@ public class SpawnEntitiesAroundPlayersAction implements IGameBehavior {
 				return posTry;
 			}
 		}
-		return BlockPos.ZERO;
+		return null;
 	}
 
 	/**
