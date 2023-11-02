1b1d2b1f2c0786e864ffd7870111e289c37770b0
Gegy <gegy.dev@gmail.com>
2023-11-02 16:29:40 +0100

    Update title strings

--- src/main/java/com/lovetropics/minigames/common/content/qottott/QottottTexts.java
+++ src/main/java/com/lovetropics/minigames/common/content/qottott/QottottTexts.java
@@ -8,7 +8,7 @@ public class QottottTexts {
 
 	static {
 		KEYS.add("sidebar.title", "Qottot");
-		KEYS.add("instruction", "Pick up coins!");
+		KEYS.add("instruction", "Pick up coins \uE040!");
 		KEYS.add("sidebar.top_players", "Top Players:");
 		KEYS.add("waiting", "Pick a kit before the game starts!");
 		KEYS.add("intro", "Pick a kit! The game will begin soon!");
@@ -23,9 +23,10 @@ public class QottottTexts {
 		KEYS.add("power_up.generic", "Power-up for %seconds% seconds");
 		KEYS.add("spawn.power_up", "%item% has spawned!");
 		KEYS.add("power_up.title", "Power-up!");
-		KEYS.add("power_up.subtitle", "%s + 2x Coins");
+		KEYS.add("power_up.subtitle", "%s + 2x \uE040");
+		KEYS.add("kill.subtitle", "+2 \uE040");
 		KEYS.add("speed_power_up", "Speed Boost Power-up");
-		KEYS.add("speed_power_up.subtitle", "Speed Boost + 2x Coins");
+		KEYS.add("speed_power_up.subtitle", "Speed Boost");
 		KEYS.add("knockback_resistance_power_up", "Knockback Resistance Power-up");
 		KEYS.add("knockback_resistance_power_up.subtitle", "Knockback Resistance");
 		KEYS.add("resistance_power_up", "Resistance Power-up");
@@ -35,9 +36,9 @@ public class QottottTexts {
 		KEYS.add("pickup_priority_power_up", "Item Pickup Priority Power-up");
 		KEYS.add("pickup_priority_power_up.subtitle", "Pick up items faster");
 		KEYS.add("cash_crab", "Cash Crab");
-		KEYS.add("cash_crab.description", "Hits drop 20% of the target's Coins");
+		KEYS.add("cash_crab.description", "Hits drop 20% of the target's \uE040");
 		KEYS.add("cash_crab.killed.title", "Oh no!");
-		KEYS.add("cash_crab.killed.subtitle", "%killer% stole 20% of your Coins!");
+		KEYS.add("cash_crab.killed.subtitle", "%killer% stole 20% of your \uE040!");
 		KEYS.add("cash_crab.leaky_pockets.title", "Oh no!");
 		KEYS.add("cash_crab.leaky_pockets.subtitle", "The Cash Crab has made your pockets leak!");
 	}
