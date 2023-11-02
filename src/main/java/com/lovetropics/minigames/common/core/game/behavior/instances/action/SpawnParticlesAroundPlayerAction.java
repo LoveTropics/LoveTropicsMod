a8eb6aafd07dad34f946d8e9a20fdf29183f25ef
Gegy <gegy.dev@gmail.com>
2023-11-02 14:16:05 +0100

    Expand particle spawner behaviour

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/SpawnParticlesAroundPlayerAction.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/action/SpawnParticlesAroundPlayerAction.java
@@ -11,38 +11,60 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
-public record SpawnParticlesAroundPlayerAction(ParticleOptions[] particles, IntProvider count, IntProvider repeats, double radius) implements IGameBehavior {
+import java.util.Optional;
+import java.util.function.Function;
+
+public record SpawnParticlesAroundPlayerAction(ParticleOptions[] particles, IntProvider count, IntProvider repeats, double radius, Vec3 offset, float speed, Optional<Vec3> position) implements IGameBehavior {
 	public static final MapCodec<SpawnParticlesAroundPlayerAction> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
 			MoreCodecs.arrayOrUnit(ParticleTypes.CODEC, ParticleOptions[]::new).fieldOf("particles").forGetter(c -> c.particles),
-			IntProvider.POSITIVE_CODEC.fieldOf("count").forGetter(c -> c.count),
+			IntProvider.POSITIVE_CODEC.optionalFieldOf("count", ConstantInt.of(1)).forGetter(c -> c.count),
 			IntProvider.POSITIVE_CODEC.optionalFieldOf("repeats", ConstantInt.of(1)).forGetter(c -> c.repeats),
-			Codec.DOUBLE.optionalFieldOf("radius", 0.0).forGetter(c -> c.radius)
+			Codec.DOUBLE.optionalFieldOf("radius", 0.0).forGetter(c -> c.radius),
+			Vec3.CODEC.optionalFieldOf("offset", new Vec3(0.1, 0.1, 0.1)).forGetter(SpawnParticlesAroundPlayerAction::offset),
+			Codec.FLOAT.optionalFieldOf("speed", 0.0f).forGetter(SpawnParticlesAroundPlayerAction::speed),
+			Vec3.CODEC.optionalFieldOf("position").forGetter(SpawnParticlesAroundPlayerAction::position)
 	).apply(i, SpawnParticlesAroundPlayerAction::new));
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) throws GameException {
+		final RandomSource random = game.getRandom();
+		final Function<ServerPlayer, Vec3> positionGenerator = createPositionGenerator(random);
 		events.listen(GameActionEvents.APPLY_TO_PLAYER, (context, player) -> {
-			RandomSource random = player.level().random;
-
 			int count = this.count.sample(random);
-			AABB bounds = player.getBoundingBox().inflate(radius);
 			for (int i = 0; i < count; i++) {
-				double x = bounds.minX + random.nextDouble() * bounds.getXsize();
-				double y = bounds.minY + random.nextDouble() * bounds.getYsize();
-				double z = bounds.minZ + random.nextDouble() * bounds.getZsize();
-
 				ParticleOptions particle = particles[random.nextInt(particles.length)];
 				int repeats = this.repeats.sample(random);
-
-				game.getLevel().sendParticles(particle, x, y, z, repeats, 0.1f, 0.1f, 0.1f, 0.0f);
+				Vec3 pos = positionGenerator.apply(player);
+				game.getLevel().sendParticles(particle, pos.x, pos.y, pos.z, repeats, offset.x, offset.y, offset.z, speed);
 			}
-
 			return true;
 		});
+	}
+
+	private Function<ServerPlayer, Vec3> createPositionGenerator(RandomSource random) {
+		if (position.isPresent()) {
+			return player -> {
+				final double deltaX = random.triangle(-radius, radius);
+				final double deltaY = random.triangle(-radius, radius);
+				final double deltaZ = random.triangle(-radius, radius);
+				return player.position().add(position.get()).add(deltaX, deltaY, deltaZ);
+			};
+		} else {
+			return player -> {
+				final AABB bounds = player.getBoundingBox().inflate(radius);
+				return new Vec3(
+						bounds.minX + random.nextDouble() * bounds.getXsize(),
+						bounds.minY + random.nextDouble() * bounds.getYsize(),
+						bounds.minZ + random.nextDouble() * bounds.getZsize()
+				);
+			};
+		}
 	}
 }
