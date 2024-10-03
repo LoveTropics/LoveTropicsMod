0d501720fec6218bcfa385961336b1da53ce8ec3
Cory Scheviak <cojomax99@gmail.com>
2024-10-03 21:32:40 +0200

    Save and restore player inventory when going from minigame to microgame and back

--- src/main/java/com/lovetropics/minigames/common/core/game/impl/MultiGamePhase.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/impl/MultiGamePhase.java
@@ -1,10 +1,17 @@
 package com.lovetropics.minigames.common.core.game.impl;
 
 import com.google.common.collect.Lists;
-import com.lovetropics.minigames.common.core.game.*;
+import com.lovetropics.minigames.common.core.game.GamePhaseType;
+import com.lovetropics.minigames.common.core.game.GameResult;
+import com.lovetropics.minigames.common.core.game.GameStopReason;
+import com.lovetropics.minigames.common.core.game.IGamePhase;
+import com.lovetropics.minigames.common.core.game.IGamePhaseDefinition;
+import com.lovetropics.minigames.common.core.game.PlayerIsolation;
 import com.lovetropics.minigames.common.core.game.behavior.BehaviorList;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameEventType;
+import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
 import com.lovetropics.minigames.common.core.game.map.GameMap;
+import com.lovetropics.minigames.common.core.game.player.PlayerRole;
 import com.lovetropics.minigames.common.core.game.rewards.GameRewardsMap;
 import com.lovetropics.minigames.common.core.game.state.GameStateMap;
 import com.lovetropics.minigames.common.core.map.MapRegions;
@@ -13,9 +20,12 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Unit;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MultiGamePhase extends GamePhase {
 
@@ -36,7 +46,7 @@ public class MultiGamePhase extends GamePhase {
         this.activePhase = activePhase;
         MultiGameManager.INSTANCE.addGamePhaseToDimension(activePhase.dimension(), activePhase);
         activePhase.state().register(GameRewardsMap.STATE, ((GameLobby) activePhase.lobby()).getRewardsMap());
-        activePhase.start();
+        activePhase.start(true);
     }
 
     public void returnHere(){
@@ -44,8 +54,20 @@ public class MultiGamePhase extends GamePhase {
         Collections.shuffle(shuffledPlayers);
 
         for (ServerPlayer player : shuffledPlayers) {
-            addAndSpawnPlayer(player, getRoleFor(player));
+            returnPlayerToParentPhase(player, getRoleFor(player));
         }
+    }
+
+    private void returnPlayerToParentPhase(ServerPlayer player, @Nullable PlayerRole role) {
+        // [Cojo] Added this event just in case we want to know when a player returns from a microgame, can remove if there's no usecase for it
+        invoker(GamePlayerEvents.RETURN).onReturn(player.getUUID(), role);
+
+        ServerPlayer newPlayer = PlayerIsolation.INSTANCE.reloadPlayerFromMemory(game, player);
+
+        invoker(GamePlayerEvents.ADD).onAdd(newPlayer);
+        invoker(GamePlayerEvents.SET_ROLE).onSetRole(newPlayer, role, null);
+
+        addedPlayers.add(player.getUUID());
     }
 
     public Set<IGamePhase> getSubPhases() {
