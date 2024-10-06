85f4292ac6bddf448ba8a595add04618054f1908
Cory Scheviak <cojomax99@gmail.com>
2024-10-06 12:32:20 +0200

    Add a configurable game queue for microgames and go from microgame to microgame before going back to the main game

--- src/main/java/com/lovetropics/minigames/common/core/game/impl/MultiGamePhase.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/impl/MultiGamePhase.java
@@ -4,18 +4,20 @@ import com.google.common.collect.Lists;
 import com.lovetropics.minigames.common.core.game.GamePhaseType;
 import com.lovetropics.minigames.common.core.game.GameResult;
 import com.lovetropics.minigames.common.core.game.GameStopReason;
-import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.IGamePhaseDefinition;
 import com.lovetropics.minigames.common.core.game.PlayerIsolation;
 import com.lovetropics.minigames.common.core.game.behavior.BehaviorList;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameEventType;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
+import com.lovetropics.minigames.common.core.game.config.GameConfig;
+import com.lovetropics.minigames.common.core.game.config.GameConfigs;
 import com.lovetropics.minigames.common.core.game.map.GameMap;
 import com.lovetropics.minigames.common.core.game.player.PlayerRole;
 import com.lovetropics.minigames.common.core.game.rewards.GameRewardsMap;
 import com.lovetropics.minigames.common.core.game.state.GameStateMap;
 import com.lovetropics.minigames.common.core.map.MapRegions;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Unit;
@@ -23,30 +25,23 @@ import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class MultiGamePhase extends GamePhase {
 
     @Nullable
     private GamePhase activePhase = null;
-    private final Set<IGamePhase> subPhases = new HashSet<>();
+    private final List<ResourceLocation> subPhaseGames = Lists.newArrayList();
 
     protected MultiGamePhase(GameInstance game, IGamePhaseDefinition definition, GamePhaseType phaseType, GameMap map, BehaviorList behaviors) {
         super(game, definition, phaseType, map, behaviors);
     }
 
-    public void addAndSetActivePhase(GamePhase activePhase){
-        subPhases.add(activePhase);
-        setActivePhase(activePhase);
-    }
-
-    public void setActivePhase(GamePhase activePhase){
+    public void setActivePhase(GamePhase activePhase, final boolean saveInventory) {
         this.activePhase = activePhase;
         MultiGameManager.INSTANCE.addGamePhaseToDimension(activePhase.dimension(), activePhase);
         activePhase.state().register(GameRewardsMap.STATE, ((GameLobby) activePhase.lobby()).getRewardsMap());
-        activePhase.start(true);
+        activePhase.start(saveInventory);
     }
 
     public void returnHere(){
@@ -70,8 +65,8 @@ public class MultiGamePhase extends GamePhase {
         addedPlayers.add(player.getUUID());
     }
 
-    public Set<IGamePhase> getSubPhases() {
-        return subPhases;
+    public List<ResourceLocation> getSubPhaseGames() {
+        return subPhaseGames;
     }
 
     @Override
@@ -102,11 +97,13 @@ public class MultiGamePhase extends GamePhase {
         // Also tick our current sub-game phase
         if(activePhase != null){
             if(activePhase.tick() != null){
-                subPhases.remove(activePhase);
                 MultiGameManager.INSTANCE.removeGamePhaseFromDimension(activePhase.dimension(), activePhase);
                 activePhase.destroy();
                 activePhase = null;
-                returnHere();
+
+                if (!startNextQueuedMicrogame(false)) {
+                    returnHere();
+                }
             }
         } else {
             return super.tick();
@@ -158,5 +155,28 @@ public class MultiGamePhase extends GamePhase {
             return;
         }
         super.destroy();
+    }
+
+    public void clearQueuedGames() {
+        subPhaseGames.clear();
+    }
+
+    public void queueGames(List<ResourceLocation> games) {
+        subPhaseGames.addAll(games);
+    }
+
+    public boolean startNextQueuedMicrogame(final boolean saveInventory) {
+        // No queued games left
+        if (subPhaseGames.isEmpty()) {
+            return false;
+        }
+
+        final ResourceLocation gameKey = subPhaseGames.removeFirst();
+        GameConfig gameConfig = GameConfigs.REGISTRY.get(gameKey);
+        GamePhase.create(game(), gameConfig.getPlayingPhase(), GamePhaseType.PLAYING).thenAccept((result) -> {
+            setActivePhase(result.getOk(), saveInventory);
+        });
+
+        return true;
     }
 }
