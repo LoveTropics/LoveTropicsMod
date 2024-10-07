c5096a05bf6e9212723610b08aba0e01528a9f76
Cory Scheviak <cojomax99@gmail.com>
2024-10-07 21:16:35 +0200

    Reward victory points for winning microgames

--- src/main/java/com/lovetropics/minigames/common/content/river_race/event/RiverRaceEvents.java
+++ src/main/java/com/lovetropics/minigames/common/content/river_race/event/RiverRaceEvents.java
@@ -1,6 +1,7 @@
 package com.lovetropics.minigames.common.content.river_race.event;
 
 import com.lovetropics.minigames.common.core.game.behavior.event.GameEventType;
+import com.lovetropics.minigames.common.core.game.impl.MultiGamePhase;
 import com.lovetropics.minigames.common.core.game.state.team.GameTeamKey;
 import net.minecraft.server.level.ServerPlayer;
 
@@ -18,11 +19,21 @@ public class RiverRaceEvents {
         }
     });
 
+    public static final GameEventType<MicrogameStarted> MICROGAME_STARTED = GameEventType.create(MicrogameStarted.class, listeners -> (game) -> {
+        for (MicrogameStarted listener : listeners) {
+            listener.onMicrogameStarted(game);
+        }
+    });
+
     public interface AnswerTriviaQuestion {
         void onAnswer(ServerPlayer player, final boolean correct);
     }
 
     public interface VictoryPointsChanged {
         void onVictoryPointsChanged(GameTeamKey team, int value, int lastValue);
+    }
+
+    public interface MicrogameStarted {
+        void onMicrogameStarted(MultiGamePhase game);
     }
 }
