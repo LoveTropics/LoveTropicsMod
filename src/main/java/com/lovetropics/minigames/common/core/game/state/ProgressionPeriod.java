ee29446400b7976944a092be0b9ac1aac6ff39ae
Gegy <gegy.dev@gmail.com>
2024-11-12 23:48:41 +0100

    Support tuple format for ProgressionPeriod

--- src/main/java/com/lovetropics/minigames/common/core/game/state/ProgressionPeriod.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/state/ProgressionPeriod.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+import java.util.List;
 import java.util.function.BooleanSupplier;
 
 public record ProgressionPeriod(ProgressionPoint start, ProgressionPoint end) {
@@ -13,7 +14,12 @@ public record ProgressionPeriod(ProgressionPoint start, ProgressionPoint end) {
 			ProgressionPoint.CODEC.fieldOf("end").forGetter(ProgressionPeriod::end)
 	).apply(i, ProgressionPeriod::new));
 
-	public static final Codec<ProgressionPeriod> CODEC = MAP_CODEC.codec();
+	private static final Codec<ProgressionPeriod> TUPLE_CODEC = ProgressionPoint.CODEC.listOf(2, 2).xmap(
+			points -> new ProgressionPeriod(points.getFirst(), points.getLast()),
+			period -> List.of(period.start, period.end)
+	);
+
+	public static final Codec<ProgressionPeriod> CODEC = Codec.withAlternative(MAP_CODEC.codec(), TUPLE_CODEC);
 
 	public BooleanSupplier createPredicate(final IGamePhase game) {
 		final GameProgressionState progression = game.state().getOrNull(GameProgressionState.KEY);
