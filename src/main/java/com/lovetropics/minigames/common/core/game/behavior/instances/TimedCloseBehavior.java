c4b82bec84710de1a278d0492d24695ac64fca97
Gegy <gegy.dev@gmail.com>
2024-11-13 12:29:16 +0100

    Allow timed_close behavior to disable triggering 'game over' or actually closing

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/TimedCloseBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/TimedCloseBehavior.java
@@ -13,10 +13,12 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import org.apache.commons.lang3.mutable.MutableBoolean;
 
-public record TimedCloseBehavior(ProgressionPoint end, ProgressionPoint close) implements IGameBehavior {
+import java.util.Optional;
+
+public record TimedCloseBehavior(Optional<ProgressionPoint> end, Optional<ProgressionPoint> close) implements IGameBehavior {
 	public static final MapCodec<TimedCloseBehavior> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
-			ProgressionPoint.CODEC.fieldOf("end").forGetter(TimedCloseBehavior::end),
-			ProgressionPoint.CODEC.fieldOf("close").forGetter(TimedCloseBehavior::close)
+			ProgressionPoint.CODEC.optionalFieldOf("end").forGetter(TimedCloseBehavior::end),
+			ProgressionPoint.CODEC.optionalFieldOf("close").forGetter(TimedCloseBehavior::close)
 	).apply(i, TimedCloseBehavior::new));
 
 	@Override
@@ -24,11 +26,11 @@ public record TimedCloseBehavior(ProgressionPoint end, ProgressionPoint close) i
 		final GameProgressionState progression = game.state().getOrThrow(GameProgressionState.KEY);
 		final MutableBoolean gameOver = new MutableBoolean();
 		events.listen(GamePhaseEvents.TICK, () -> {
-			if (!gameOver.getValue() && progression.isAfter(end)) {
+			if (end.isPresent() && !gameOver.getValue() && progression.isAfter(end.get())) {
 				game.invoker(GameLogicEvents.GAME_OVER).onGameOver();
 				gameOver.setTrue();
 			}
-			if (progression.isAfter(close)) {
+			if (close.isPresent() && progression.isAfter(close.get())) {
 				game.requestStop(GameStopReason.finished());
 			}
 		});
