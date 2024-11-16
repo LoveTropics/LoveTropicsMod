9baa65d2364a1cdbce415c5f79259e2db4c18b79
Gegy <gegy.dev@gmail.com>
2024-11-16 22:13:30 +0100

    Generify scale_damage_from_entity to take a DamageSourcePredicate

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/GameBehaviorTypes.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/GameBehaviorTypes.java
@@ -126,7 +126,7 @@ import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.Disab
 import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.DisableThrowingItemsBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.DisableTntDestructionBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.PreventBreakBehavior;
-import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.ScaleDamageFromEntityBehavior;
+import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.ScaleDamageBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.ScaleExplosionKnockbackBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.ScalePlayerDamageBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.instances.tweak.SetDayTimeBehavior;
@@ -183,7 +183,7 @@ public class GameBehaviorTypes {
 	public static final GameBehaviorEntry<BindControlsBehavior> BIND_CONTROLS = register("bind_controls", BindControlsBehavior.CODEC);
 	public static final GameBehaviorEntry<CancelPlayerDamageBehavior> CANCEL_PLAYER_DAMAGE = register("cancel_player_damage", CancelPlayerDamageBehavior.CODEC);
 	public static final GameBehaviorEntry<ScalePlayerDamageBehavior> SCALE_PLAYER_DAMAGE = register("scale_player_damage", ScalePlayerDamageBehavior.CODEC);
-	public static final GameBehaviorEntry<ScaleDamageFromEntityBehavior> SCALE_DAMAGE_FROM_ENTITY = register("scale_damage_from_entity", ScaleDamageFromEntityBehavior.CODEC);
+	public static final GameBehaviorEntry<ScaleDamageBehavior> SCALE_DAMAGE = register("scale_damage", ScaleDamageBehavior.CODEC);
 	public static final GameBehaviorEntry<ScaleExplosionKnockbackBehavior> SCALE_EXPLOSION_KNOCKBACK = register("scale_explosion_knockback", ScaleExplosionKnockbackBehavior.CODEC);
 	public static final GameBehaviorEntry<SetGameRulesBehavior> SET_GAME_RULES = register("set_game_rules", SetGameRulesBehavior.CODEC);
 	public static final GameBehaviorEntry<SetupTeamsBehavior> SETUP_TEAMS = register("setup_teams", SetupTeamsBehavior.CODEC);
--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/tweak/ScaleDamageBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/tweak/ScaleDamageBehavior.java
@@ -5,21 +5,21 @@ import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvents;
-import com.lovetropics.minigames.common.core.game.predicate.entity.EntityPredicate;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.advancements.critereon.DamageSourcePredicate;
 
-public record ScaleDamageFromEntityBehavior(float factor, EntityPredicate predicate) implements IGameBehavior {
-    public static final MapCodec<ScaleDamageFromEntityBehavior> CODEC = RecordCodecBuilder.mapCodec(in -> in.group(
-            Codec.floatRange(0.0F, Float.MAX_VALUE).fieldOf("factor").forGetter(ScaleDamageFromEntityBehavior::factor),
-            EntityPredicate.CODEC_WITH_FALLBACK.fieldOf("source").forGetter(ScaleDamageFromEntityBehavior::predicate)
-    ).apply(in, ScaleDamageFromEntityBehavior::new));
+public record ScaleDamageBehavior(float factor, DamageSourcePredicate source) implements IGameBehavior {
+    public static final MapCodec<ScaleDamageBehavior> CODEC = RecordCodecBuilder.mapCodec(in -> in.group(
+            Codec.floatRange(0.0F, Float.MAX_VALUE).fieldOf("factor").forGetter(ScaleDamageBehavior::factor),
+            DamageSourcePredicate.CODEC.fieldOf("source").forGetter(ScaleDamageBehavior::source)
+    ).apply(in, ScaleDamageBehavior::new));
 
     @Override
     public void register(IGamePhase game, EventRegistrar events) throws GameException {
         events.listen(GamePlayerEvents.DAMAGE_AMOUNT, (player, damageSource, amount, originalAmount) -> {
-            if (damageSource.getDirectEntity() != null && predicate.test(damageSource.getDirectEntity())) {
+            if (source.matches(player, damageSource)) {
                 return factor * amount;
             }
             return amount;
