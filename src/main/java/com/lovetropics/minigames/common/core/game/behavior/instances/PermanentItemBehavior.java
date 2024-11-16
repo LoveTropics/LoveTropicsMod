0f1469d76166a1cb80661abcaf5a34ec8acdf6ea
Gegy <gegy.dev@gmail.com>
2024-11-16 22:22:37 +0100

    Allow components in permanent_item, spawn Eruptive Spleef players in adventure mode

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/PermanentItemBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/PermanentItemBehavior.java
@@ -1,5 +1,6 @@
 package com.lovetropics.minigames.common.core.game.behavior.instances;
 
+import com.lovetropics.lib.codec.MoreCodecs;
 import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
@@ -7,14 +8,11 @@ import com.lovetropics.minigames.common.core.game.behavior.event.GamePlayerEvent
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
-public record PermanentItemBehavior(Item item, int count, int interval) implements IGameBehavior {
+public record PermanentItemBehavior(ItemStack item, int interval) implements IGameBehavior {
 	public static final MapCodec<PermanentItemBehavior> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
-			BuiltInRegistries.ITEM.byNameCodec().fieldOf("item").forGetter(c -> c.item),
-			Codec.INT.fieldOf("count").forGetter(c -> c.count),
+			MoreCodecs.ITEM_STACK.fieldOf("item").forGetter(c -> c.item),
 			Codec.INT.optionalFieldOf("interval", 5).forGetter(c -> c.interval)
 	).apply(i, PermanentItemBehavior::new));
 
@@ -22,9 +20,10 @@ public record PermanentItemBehavior(Item item, int count, int interval) implemen
 	public void register(IGamePhase game, EventRegistrar events) {
 		events.listen(GamePlayerEvents.TICK, player -> {
 			if (game.participants().contains(player) && game.ticks() % interval == 0) {
-				int currentCount = player.getInventory().countItem(item);
-				if (currentCount < count) {
-					player.getInventory().add(new ItemStack(item, count - currentCount));
+				int currentCount = player.getInventory().countItem(item.getItem());
+				int targetCount = item.getCount();
+				if (currentCount < targetCount) {
+					player.getInventory().add(item.copyWithCount(targetCount - currentCount));
 				}
 			}
 		});
