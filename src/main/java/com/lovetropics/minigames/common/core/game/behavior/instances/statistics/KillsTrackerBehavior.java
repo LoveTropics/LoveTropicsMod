f06666995bf265c31cb6b21dc3a929974e073c1e
Gegy <gegy.dev@gmail.com>
2024-11-17 14:10:07 +0100

    Track kills per team in Pillars of Wealth, show in sidebar

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/statistics/KillsTrackerBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/statistics/KillsTrackerBehavior.java
@@ -8,6 +8,8 @@ import com.lovetropics.minigames.common.core.game.state.statistics.GameStatistic
 import com.lovetropics.minigames.common.core.game.state.statistics.PlayerKey;
 import com.lovetropics.minigames.common.core.game.state.statistics.StatisticKey;
 import com.lovetropics.minigames.common.core.game.state.statistics.StatisticsMap;
+import com.lovetropics.minigames.common.core.game.state.team.GameTeamKey;
+import com.lovetropics.minigames.common.core.game.state.team.TeamState;
 import com.lovetropics.minigames.common.util.Util;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.server.level.ServerPlayer;
@@ -18,15 +20,19 @@ public final class KillsTrackerBehavior implements IGameBehavior {
 
 	@Override
 	public void register(IGamePhase game, EventRegistrar events) {
+		TeamState teams = game.instanceState().getOrNull(TeamState.KEY);
 		events.listen(GamePlayerEvents.DEATH, (player, damageSource) -> {
 			GameStatistics statistics = game.statistics();
 			StatisticsMap playerStatistics = statistics.forPlayer(player);
 
 			final ServerPlayer killerPlayer = Util.getKillerPlayer(player, damageSource);
 			if (killerPlayer != null) {
-				statistics.forPlayer(killerPlayer)
-						.withDefault(StatisticKey.KILLS, () -> 0)
-						.apply(kills -> kills + 1);
+				statistics.forPlayer(killerPlayer).incrementInt(StatisticKey.KILLS, 1);
+
+				GameTeamKey team = teams != null ? teams.getTeamForPlayer(killerPlayer) : null;
+				if (team != null) {
+					statistics.forTeam(team).incrementInt(StatisticKey.KILLS, 1);
+				}
 
 				playerStatistics.set(StatisticKey.KILLED_BY, PlayerKey.from(killerPlayer));
 			}
