35f04432d54b368826abe690a6c44ac0bc17a26a
Gegy <gegy.dev@gmail.com>
2024-11-19 00:27:48 +0100

    Add teams dodgeball

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/statistics/SumStatisticBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/statistics/SumStatisticBehavior.java
@@ -8,27 +8,45 @@ import com.lovetropics.minigames.common.core.game.behavior.event.GamePhaseEvents
 import com.lovetropics.minigames.common.core.game.state.statistics.GameStatistics;
 import com.lovetropics.minigames.common.core.game.state.statistics.PlayerKey;
 import com.lovetropics.minigames.common.core.game.state.statistics.StatisticKey;
+import com.lovetropics.minigames.common.core.game.state.team.GameTeam;
 import com.lovetropics.minigames.common.core.game.state.team.GameTeamKey;
+import com.lovetropics.minigames.common.core.game.state.team.TeamState;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.server.level.ServerPlayer;
 
-public record SumStatisticBehavior(StatisticKey<Integer> statistic) implements IGameBehavior {
+public record SumStatisticBehavior(StatisticKey<Integer> statistic, boolean forTeam) implements IGameBehavior {
 	public static final MapCodec<SumStatisticBehavior> CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
-			StatisticKey.INT_CODEC.fieldOf("statistic").forGetter(SumStatisticBehavior::statistic)
+			StatisticKey.INT_CODEC.fieldOf("statistic").forGetter(SumStatisticBehavior::statistic),
+			Codec.BOOL.optionalFieldOf("for_team", false).forGetter(SumStatisticBehavior::forTeam)
 	).apply(i, SumStatisticBehavior::new));
 
 	@Override
-	public void register(final IGamePhase game, final EventRegistrar events) throws GameException {
+	public void register(IGamePhase game, EventRegistrar events) throws GameException {
+		GameStatistics statistics = game.statistics();
 		events.listen(GamePhaseEvents.FINISH, () -> {
-			final GameStatistics statistics = game.statistics();
 			int total = 0;
-			for (final PlayerKey player : statistics.getPlayers()) {
+			for (PlayerKey player : statistics.getPlayers()) {
 				total += statistics.forPlayer(player).getInt(statistic);
 			}
-			for (final GameTeamKey team : statistics.getTeams()) {
+			for (GameTeamKey team : statistics.getTeams()) {
 				total += statistics.forTeam(team).getInt(statistic);
 			}
 			statistics.global().set(statistic, total);
 		});
+
+		if (forTeam) {
+			TeamState teams = game.instanceState().getOrThrow(TeamState.KEY);
+			events.listen(GamePhaseEvents.TICK, () -> {
+				for (GameTeam team : teams) {
+					int total = 0;
+					for (ServerPlayer player : teams.getPlayersForTeam(team.key())) {
+						total += statistics.forPlayer(player).getInt(statistic);
+					}
+					statistics.forTeam(team.key()).set(statistic, total);
+				}
+			});
+		}
 	}
 }
