9d1b1ae8821005d94f0e68aa4f0f7003f1582f15
Gegy <gegy.dev@gmail.com>
2024-11-20 03:04:28 +0100

    Fix: use MoreCodecs.TIME_CODEC that accepts more date formats

--- src/main/java/com/lovetropics/minigames/common/core/integration/game_actions/GameActionRequest.java
+++ src/main/java/com/lovetropics/minigames/common/core/integration/game_actions/GameActionRequest.java
@@ -6,17 +6,14 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.UUIDUtil;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.Instant;
 import java.util.UUID;
 
-public record GameActionRequest(GameActionType type, UUID uuid, LocalDateTime time, GameAction action) {
-	private static final Codec<LocalDateTime> TIME_CODEC = MoreCodecs.localDateTime(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
-
+public record GameActionRequest(GameActionType type, UUID uuid, Instant time, GameAction action) {
 	public static Codec<GameActionRequest> codec(GameActionType type, MapCodec<GameAction> codec) {
 		return RecordCodecBuilder.create(i -> i.group(
 				UUIDUtil.STRING_CODEC.fieldOf("uuid").forGetter(GameActionRequest::uuid),
-				TIME_CODEC.fieldOf(type.getTimeFieldName()).forGetter(GameActionRequest::time),
+				MoreCodecs.TIME_CODEC.fieldOf(type.getTimeFieldName()).forGetter(GameActionRequest::time),
 				codec.forGetter(GameActionRequest::action)
 		).apply(i, (uuid, triggerTime, action) -> new GameActionRequest(type, uuid, triggerTime, action)));
 	}
