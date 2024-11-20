f3746c05798be391aef2c060e5a37cd5a95733f4
Gegy <gegy.dev@gmail.com>
2024-11-20 13:39:34 +0100

    Scale knockback from Wind Charges in Crab Hockey

--- src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/tweak/ScaleExplosionKnockbackBehavior.java
+++ src/main/java/com/lovetropics/minigames/common/core/game/behavior/instances/tweak/ScaleExplosionKnockbackBehavior.java
@@ -5,24 +5,34 @@ import com.lovetropics.minigames.common.core.game.IGamePhase;
 import com.lovetropics.minigames.common.core.game.behavior.IGameBehavior;
 import com.lovetropics.minigames.common.core.game.behavior.event.EventRegistrar;
 import com.lovetropics.minigames.common.core.game.behavior.event.GameLivingEntityEvents;
-import com.lovetropics.minigames.common.core.game.predicate.entity.EntityPredicate;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.advancements.critereon.EntityPredicate;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Explosion;
 
-public record ScaleExplosionKnockbackBehavior(float factor, EntityPredicate exploderPredicate) implements IGameBehavior {
+public record ScaleExplosionKnockbackBehavior(float factor, EntityPredicate explodedPredicate, EntityPredicate exploderPredicate) implements IGameBehavior {
     public static final MapCodec<ScaleExplosionKnockbackBehavior> CODEC = RecordCodecBuilder.mapCodec(in -> in.group(
             Codec.floatRange(0.0F, Float.MAX_VALUE).fieldOf("factor").forGetter(ScaleExplosionKnockbackBehavior::factor),
-            EntityPredicate.CODEC_WITH_FALLBACK.fieldOf("exploder").forGetter(ScaleExplosionKnockbackBehavior::exploderPredicate)
+            EntityPredicate.CODEC.fieldOf("exploded").forGetter(ScaleExplosionKnockbackBehavior::explodedPredicate),
+            EntityPredicate.CODEC.fieldOf("exploder").forGetter(ScaleExplosionKnockbackBehavior::exploderPredicate)
     ).apply(in, ScaleExplosionKnockbackBehavior::new));
 
     @Override
     public void register(IGamePhase game, EventRegistrar events) throws GameException {
         events.listen(GameLivingEntityEvents.MODIFY_EXPLOSION_KNOCKBACK, (entity, explosion, knockback, originalKnockback) -> {
-            if (explosion.getDirectSourceEntity() != null && exploderPredicate.test(explosion.getDirectSourceEntity())) {
+            if (matches(game, entity, explosion)) {
                 return knockback.scale(factor);
             }
             return knockback;
-        });
+		});
     }
+
+    private boolean matches(IGamePhase game, Entity entity, Explosion explosion) {
+        if (!explodedPredicate.matches(game.level(), explosion.center(), entity)) {
+            return false;
+        }
+		return explosion.getDirectSourceEntity() != null && exploderPredicate.matches(game.level(), explosion.center(), explosion.getDirectSourceEntity());
+	}
 }
